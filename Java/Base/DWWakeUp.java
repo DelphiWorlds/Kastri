b45cf6d67d33e1d57a4e222f430fb646491db0fb
Dave Nottage <davidn@radsoft.com.au>
2020-06-25 11:42:52 +0930

    Added allowance for forcing wake lock check

--- Java/Base/DWWakeUp.java
+++ Java/Base/DWWakeUp.java
@@ -24,7 +24,7 @@ public class DWWakeUp {
 
   private static final String TAG = "DWWakeUp";
 
-  public static void checkWakeUp(Context context, String metaDataKey) {
+  public static void checkWakeUp(Context context, String metaDataKey, boolean force) {
     Bundle metaData = null;
     try {
       metaData = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA).metaData;
@@ -32,11 +32,13 @@ public class DWWakeUp {
       Log.w(TAG, "Unable to load metadata");
     }
     Log.d(TAG, "Checking metadata for " + metaDataKey);
-    if ((metaData == null) || (!metaData.containsKey(metaDataKey) || !metaData.getBoolean(metaDataKey)))
+    if ((metaData == null) || (!metaData.containsKey(metaDataKey) || !metaData.getBoolean(metaDataKey))) {
       return;
+    }
     PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
     boolean isScreenOn = Build.VERSION.SDK_INT >= 20 ? powerManager.isInteractive() : powerManager.isScreenOn();
-    if (!isScreenOn) {
+    if (!isScreenOn || force) {
+      Log.d(TAG, "Acquiring wake lock");
       String wakeLockTag = context.getPackageName() + "." + TAG + "WakeLock";
       PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, wakeLockTag);
       if (!wakeLock.isHeld())
