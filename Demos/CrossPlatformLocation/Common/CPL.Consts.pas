f0453c1657f5168777ca68e8038fb6c9b6ae398b
Dave Nottage <davidn@radsoft.com.au>
2020-07-01 14:52:16 +0930

    Added support for sending messages from service

--- Demos/CrossPlatformLocation/Common/CPL.Consts.pas
+++ Demos/CrossPlatformLocation/Common/CPL.Consts.pas
@@ -5,6 +5,10 @@ interface
 const
   cServiceName = 'CrossPlatformLocationService';
 
+  cServiceCommandAction = 'com.delphiworlds.action.SERVICE_COMMAND';
+  cServiceMessageAction = 'com.delphiworlds.action.SERVICE_MESSAGE';
+  cServiceBroadcastParamMessage = 'MESSAGE';
+  cServiceBroadcastParamCommand = 'COMMAND';
   cServiceCommandStartLocationUpdates = 100;
   cServiceCommandStopLocationUpdates = 101;
 
