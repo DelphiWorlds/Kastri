41a043ee4474f4202c5d01e8d911e2a0c413b659
Dave Nottage <davidn@radsoft.com.au>
2020-10-15 09:34:08 +1030

    Updated for 10.4.1

--- Demos/Connectivity/Unit1.pas
+++ Demos/Connectivity/Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.Memo.Types,
   DW.Connectivity;
 
 type
