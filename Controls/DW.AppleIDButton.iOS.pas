12937d45d89d7dbb992f9c5de21f22dc14567c12
Dave Nottage <davidn@radsoft.com.au>
2020-11-06 10:00:38 +1030

    Added AppleIDButton

---
+++ Controls/DW.AppleIDButton.iOS.pas
@@ -0,0 +1,135 @@
+unit DW.AppleIDButton.iOS;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{    Copyright 2020 Dave Nottage under MIT license      }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  // RTL
+  System.TypInfo,
+  // iOS
+  iOSapi.Foundation,
+  // FMX
+  FMX.Presentation.iOS, FMX.Presentation.Messages, FMX.Controls.Model,
+  // DW
+  DW.AppleIDButton, DW.iOSapi.AuthenticationServices;
+
+type
+  IAppleIDButton = interface(ASAuthorizationAppleIDButton)
+    ['{332B340D-21DF-4C0E-86A5-B3418ED3D9FF}']
+    procedure ButtonClicked(sender: Pointer); cdecl;
+  end;
+
+  TiOSAppleIDButton = class(TiOSNativeControl)
+  private
+    function GetModel: TAppleIDButtonModel;
+    function GetView: ASAuthorizationAppleIDButton;
+  protected
+    function DefineModelClass: TDataModelClass; override;
+    function GetObjectiveCClass: PTypeInfo; override;
+    procedure InitView; override;
+    procedure MMSetButtonStyle(var AMessage: TDispatchMessageWithValue<TAppleIDButtonStyle>); message MM_SET_BUTTON_STYLE;
+    procedure MMSetButtonType(var AMessage: TDispatchMessageWithValue<TAppleIDButtonType>); message MM_SET_BUTTON_TYPE;
+    procedure MMSetCornerRadius(var AMessage: TDispatchMessageWithValue<Double>); message MM_SET_CORNER_RADIUS;
+  public
+    { IAppleIDButton }
+    procedure ButtonClicked(sender: Pointer); cdecl;
+  public
+    constructor Create; override;
+    property Model: TAppleIDButtonModel read GetModel;
+    property View: ASAuthorizationAppleIDButton read GetView;
+  end;
+
+implementation
+
+uses
+  // iOS
+  iOSapi.UIKit,
+  // FMX
+  FMX.Presentation.Factory, FMX.Controls,
+  // DW
+  DW.OSLog,
+  DW.AuthenticationServices.iOS;
+
+{ TiOSAppleIDButton }
+
+constructor TiOSAppleIDButton.Create;
+begin
+  inherited;
+  RegisterNativeEventHandler('ButtonClicked:', UIControlEventTouchDown);
+end;
+
+function TiOSAppleIDButton.DefineModelClass: TDataModelClass;
+begin
+  Result := TAppleIDButtonModel;
+end;
+
+function TiOSAppleIDButton.GetModel: TAppleIDButtonModel;
+begin
+  Result := inherited GetModel<TAppleIDButtonModel>;
+end;
+
+function TiOSAppleIDButton.GetObjectiveCClass: PTypeInfo;
+begin
+  Result := TypeInfo(IAppleIDButton);
+end;
+
+function TiOSAppleIDButton.GetView: ASAuthorizationAppleIDButton;
+begin
+  Result := inherited GetView<ASAuthorizationAppleIDButton>;
+end;
+
+procedure TiOSAppleIDButton.InitView;
+const
+  cButtonStyles: array[TAppleIDButtonStyle] of ASAuthorizationAppleIDButtonStyle = (
+    ASAuthorizationAppleIDButtonStyleWhite, ASAuthorizationAppleIDButtonStyleWhiteOutline, ASAuthorizationAppleIDButtonStyleBlack
+  );
+  cButtonTypes: array[TAppleIDButtonType] of ASAuthorizationAppleIDButtonType = (
+    ASAuthorizationAppleIDButtonTypeSignIn, ASAuthorizationAppleIDButtonTypeContinue, ASAuthorizationAppleIDButtonTypeSignUp
+  );
+var
+  LViewPtr: Pointer;
+begin
+  LViewPtr := View.initWithAuthorizationButtonType(cButtonTypes[Model.ButtonType], cButtonStyles[Model.ButtonStyle]);
+  if GetObjectID <> LViewPtr then
+    UpdateObjectID(LViewPtr);
+end;
+
+procedure TiOSAppleIDButton.MMSetButtonStyle(var AMessage: TDispatchMessageWithValue<TAppleIDButtonStyle>);
+begin
+  InitView;
+end;
+
+procedure TiOSAppleIDButton.MMSetButtonType(var AMessage: TDispatchMessageWithValue<TAppleIDButtonType>);
+begin
+  InitView;
+end;
+
+procedure TiOSAppleIDButton.MMSetCornerRadius(var AMessage: TDispatchMessageWithValue<Double>);
+begin
+  View.setCornerRadius(AMessage.Value);
+end;
+
+procedure TiOSAppleIDButton.ButtonClicked(sender: Pointer);
+begin
+  TAuthenticationServices.Current.RequestAuthorization;
+end;
+
+initialization
+  TPresentationProxyFactory.Current.Register(TAppleIDButton, TControlType.Platform, TiOSPresentationProxy<TiOSAppleIDButton>);
+
+finalization
+  TPresentationProxyFactory.Current.Unregister(TAppleIDButton, TControlType.Platform, TiOSPresentationProxy<TiOSAppleIDButton>);
+
+end.
---
+++ Controls/DW.AppleIDButton.pas
@@ -0,0 +1,184 @@
+unit DW.AppleIDButton;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{    Copyright 2020 Dave Nottage under MIT license      }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  // RTL
+  System.Messaging, System.Classes, System.Types,
+  // FMX
+  FMX.StdCtrls, FMX.Controls.Presentation, FMX.Controls.Model,
+  // DW
+  DW.AuthenticationServices.Types;
+
+const
+  MM_SET_CORNER_RADIUS = MM_USER + 1;
+  MM_SET_BUTTON_STYLE = MM_USER + 2;
+  MM_SET_BUTTON_TYPE = MM_USER + 3;
+
+type
+  TAppleIDAuthorizationResponseEvent = procedure(Sender: TObject; const Response: TAppleIDAuthorizationResponse) of object;
+
+  TAppleIDButtonType = (SignIn, Continue, SignUp);
+  TAppleIDButtonStyle = (White, WhiteOutline, Black);
+
+  TAppleIDButtonModel = class(TDataModel)
+  private
+    FButtonStyle: TAppleIDButtonStyle;
+    FButtonType: TAppleIDButtonType;
+    FCornerRadius: Double;
+    procedure SetCornerRadius(const Value: Double);
+    procedure SetButtonStyle(const Value: TAppleIDButtonStyle);
+    procedure SetButtonType(const Value: TAppleIDButtonType);
+  public
+    property ButtonStyle: TAppleIDButtonStyle read FButtonStyle write SetButtonStyle;
+    property ButtonType: TAppleIDButtonType read FButtonType write SetButtonType;
+    property CornerRadius: Double read FCornerRadius write SetCornerRadius;
+  end;
+
+  TAppleIDButton = class(TButton)
+  private
+    FOnAuthorizationResponse: TAppleIDAuthorizationResponseEvent;
+    procedure AppleIDAuthorizationResponseMessageHandler(const Sender: TObject; const AMsg: TMessage);
+    function GetModel: TAppleIDButtonModel; overload;
+    procedure SetCornerRadius(const Value: Double);
+    procedure SetButtonStyle(const Value: TAppleIDButtonStyle);
+    procedure SetButtonType(const Value: TAppleIDButtonType);
+    function GetButtonStyle: TAppleIDButtonStyle;
+    function GetButtonType: TAppleIDButtonType;
+    function GetCornerRadius: Double;
+  protected
+    function DefineModelClass: TDataModelClass; override;
+    function RecommendSize(const AWishedSize: TSizeF): TSizeF; override;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    property ButtonStyle: TAppleIDButtonStyle read GetButtonStyle write SetButtonStyle;
+    property ButtonType: TAppleIDButtonType read GetButtonType write SetButtonType;
+    property CornerRadius: Double read GetCornerRadius write SetCornerRadius;
+    property Model: TAppleIDButtonModel read GetModel;
+    property OnAuthorizationResponse: TAppleIDAuthorizationResponseEvent read FOnAuthorizationResponse write FOnAuthorizationResponse;
+  end;
+
+implementation
+
+{$IF Defined(IOS)}
+uses
+  // DW
+  DW.OSLog,
+  DW.AppleIDButton.iOS;
+{$ENDIF}
+
+{ TAppleIDButtonModel }
+
+procedure TAppleIDButtonModel.SetButtonStyle(const Value: TAppleIDButtonStyle);
+begin
+  if Value <> FButtonStyle then
+  begin
+    FButtonStyle := Value;
+    SendMessage<TAppleIDButtonStyle>(MM_SET_BUTTON_STYLE, FButtonStyle);
+  end;
+end;
+
+procedure TAppleIDButtonModel.SetButtonType(const Value: TAppleIDButtonType);
+begin
+  if Value <> FButtonType then
+  begin
+    FButtonType := Value;
+    SendMessage<TAppleIDButtonType>(MM_SET_BUTTON_STYLE, FButtonType);
+  end;
+end;
+
+procedure TAppleIDButtonModel.SetCornerRadius(const Value: Double);
+begin
+  if Value <> FCornerRadius then
+  begin
+    FCornerRadius := Value;
+    SendMessage<Double>(MM_SET_CORNER_RADIUS, FCornerRadius);
+  end;
+end;
+
+{ TAppleIDButton }
+
+constructor TAppleIDButton.Create(AOwner: TComponent);
+begin
+  inherited;
+  ControlType := TControlType.Platform;
+  Width := 200;
+  Height := 50;
+  TMessageManager.DefaultManager.SubscribeToMessage(TAppleIDAuthorizationResponseMessage, AppleIDAuthorizationResponseMessageHandler);
+end;
+
+destructor TAppleIDButton.Destroy;
+begin
+  TMessageManager.DefaultManager.Unsubscribe(TAppleIDAuthorizationResponseMessage, AppleIDAuthorizationResponseMessageHandler);
+  inherited;
+end;
+
+function TAppleIDButton.DefineModelClass: TDataModelClass;
+begin
+  Result := TAppleIDButtonModel;
+end;
+
+function TAppleIDButton.GetButtonStyle: TAppleIDButtonStyle;
+begin
+  Result := Model.ButtonStyle;
+end;
+
+function TAppleIDButton.GetButtonType: TAppleIDButtonType;
+begin
+  Result := Model.ButtonType;
+end;
+
+function TAppleIDButton.GetCornerRadius: Double;
+begin
+  Result := Model.CornerRadius;
+end;
+
+function TAppleIDButton.GetModel: TAppleIDButtonModel;
+begin
+  Result := GetModel<TAppleIDButtonModel>;
+end;
+
+function TAppleIDButton.RecommendSize(const AWishedSize: TSizeF): TSizeF;
+begin
+  Result := AWishedSize;
+end;
+
+procedure TAppleIDButton.SetButtonStyle(const Value: TAppleIDButtonStyle);
+begin
+  Model.ButtonStyle := Value;
+end;
+
+procedure TAppleIDButton.SetButtonType(const Value: TAppleIDButtonType);
+begin
+  Model.ButtonType := Value;
+end;
+
+procedure TAppleIDButton.SetCornerRadius(const Value: Double);
+begin
+  Model.CornerRadius := Value;
+end;
+
+procedure TAppleIDButton.AppleIDAuthorizationResponseMessageHandler(const Sender: TObject; const AMsg: TMessage);
+var
+  LResponse: TAppleIDAuthorizationResponse;
+begin
+  LResponse := TAppleIDAuthorizationResponseMessage(AMsg).Value;
+  if Assigned(FOnAuthorizationResponse) then
+    FOnAuthorizationResponse(Self, LResponse);
+end;
+
+end.
