7eacbc6f486593b6a97a9a85f3c4663f21e2b3a0
Dave Nottage <davidn@radsoft.com.au>
2020-11-10 12:45:51 +1030

    Added full screen YouTube video support

--- Java/WebChromeClient/DWWebChromeClient.java
+++ Java/WebChromeClient/DWWebChromeClient.java
@@ -11,25 +11,46 @@ package com.delphiworlds.kastri;
  *                                                     *
  *******************************************************/
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
 import android.webkit.WebChromeClient;
 import android.webkit.WebChromeClient.FileChooserParams;
 import android.webkit.WebView;
 import android.webkit.PermissionRequest;
 import android.webkit.ValueCallback;
+import android.widget.FrameLayout;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DWWebChromeClient extends WebChromeClient {
 
+  private static final int FULL_SCREEN_SETTING = View.SYSTEM_UI_FLAG_FULLSCREEN |
+    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+    View.SYSTEM_UI_FLAG_IMMERSIVE;
+  private Activity mActivity;
+  private View mCustomView;
+  private WebChromeClient.CustomViewCallback mCustomViewCallback;
   private DWWebChromeClientDelegate mDelegate;
   private ValueCallback<Uri[]> mFilePathCallback;
+  private int mOriginalOrientation;
+  private int mOriginalSystemUiVisibility;
 
   public DWWebChromeClient(DWWebChromeClientDelegate delegate) {
     mDelegate = delegate;
+  }
+
+  public DWWebChromeClient(DWWebChromeClientDelegate delegate, Activity activity) {
+    mDelegate = delegate;
+    mActivity = activity;
   }
 
   // https://stackoverflow.com/a/55449804/3164070
@@ -59,8 +80,43 @@ public class DWWebChromeClient extends WebChromeClient {
   }
 
   @Override
+  public void onHideCustomView() {
+    ((FrameLayout) mActivity.getWindow().getDecorView()).removeView(mCustomView);
+    mCustomView = null;
+    mActivity.getWindow().getDecorView().setSystemUiVisibility(mOriginalSystemUiVisibility);
+    mActivity.setRequestedOrientation(mOriginalOrientation);
+    mCustomViewCallback.onCustomViewHidden();
+    mCustomViewCallback = null;
+    mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
+  }
+
+  @Override
   public void onPermissionRequest(final PermissionRequest request) {
     request.grant(request.getResources());
+  }
+
+  @Override
+  public void onShowCustomView(View webView, WebChromeClient.CustomViewCallback viewCallback) {
+    if (mActivity == null)
+      return;
+    if (mCustomView != null) {
+      onHideCustomView();
+      return;
+    }
+    mCustomView = webView;
+    mOriginalSystemUiVisibility = mActivity.getWindow().getDecorView().getSystemUiVisibility();
+    mOriginalOrientation = mActivity.getRequestedOrientation();
+    mCustomViewCallback = viewCallback;
+    ((FrameLayout) mActivity.getWindow().getDecorView())
+      .addView(mCustomView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    mActivity.getWindow().getDecorView().setSystemUiVisibility(FULL_SCREEN_SETTING);
+    mActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
+    mCustomView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+      @Override
+      public void onSystemUiVisibilityChange(int visibility) {
+        updateControls();
+      }
+    }); // visibility -> updateControls() // Supported in Java 1.8+
   }
 
   // Gleaned from: https://github.com/anthonycr/Lightning-Browser/issues/253
@@ -78,5 +134,17 @@ public class DWWebChromeClient extends WebChromeClient {
     if (mFilePathCallback != null)
       mFilePathCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
     mFilePathCallback = null;
+  }
+
+  private void updateControls() {
+    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) mCustomView.getLayoutParams();
+    params.bottomMargin = 0;
+    params.topMargin = 0;
+    params.leftMargin = 0;
+    params.rightMargin = 0;
+    params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+    mCustomView.setLayoutParams(params);
+    mActivity.getWindow().getDecorView().setSystemUiVisibility(FULL_SCREEN_SETTING);
   }
 }
