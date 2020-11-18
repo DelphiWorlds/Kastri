bf9afd1f3015f35c72957ad24e995afa48c0cc69
Dave Nottage <davidn@radsoft.com.au>
2020-11-19 05:34:23 +1030

    Resolve missing properties issue

--- Features/Notifications/DW.Notifications.Android.pas
+++ Features/Notifications/DW.Notifications.Android.pas
@@ -24,10 +24,139 @@ interface
 uses
   // Android
   Androidapi.JNI.App, Androidapi.JNI.GraphicsContentViewText, Androidapi.JNI.JavaTypes, Androidapi.JNIBridge,
+  {$IF CompilerVersion > 32}
+  Androidapi.JNI.Os, Androidapi.JNI.Net, Androidapi.JNI.Media,
+  {$ENDIF}
   // DW
   DW.Notifications, DW.MultiReceiver.Android, DW.Androidapi.JNI.App, DW.Androidapi.JNI.SupportV4;
 
 type
+  {$IF CompilerVersion > 32}
+  JNotificationChannel = interface;
+  JNotificationChannelGroup = interface;
+  JNotificationManager = interface;
+
+  JNotificationChannelClass = interface(JObjectClass)
+    ['{2C435911-AD3A-4461-8860-779ECA70D332}']
+    function _GetCREATOR: JParcelable_Creator; cdecl;
+    function _GetDEFAULT_CHANNEL_ID: JString; cdecl;
+    function init(id: JString; name: JCharSequence; importance: Integer): JNotificationChannel; cdecl;
+    property CREATOR: JParcelable_Creator read _GetCREATOR;
+    property DEFAULT_CHANNEL_ID: JString read _GetDEFAULT_CHANNEL_ID;
+  end;
+
+  [JavaSignature('android/app/NotificationChannel')]
+  JNotificationChannel = interface(JObject)
+    ['{3AA9585E-BF01-40A2-933E-73DFEB241BB9}']
+    function canBypassDnd: boolean; cdecl;
+    function canShowBadge: boolean; cdecl;
+    function describeContents: Integer; cdecl;
+    procedure enableLights(lights: boolean); cdecl;
+    procedure enableVibration(vibration: boolean); cdecl;
+    function equals(o: JObject): boolean; cdecl;
+    function getAudioAttributes: JAudioAttributes; cdecl;
+    function getDescription: JString; cdecl;
+    function getGroup: JString; cdecl;
+    function getId: JString; cdecl;
+    function getImportance: Integer; cdecl;
+    function getLightColor: Integer; cdecl;
+    function getLockscreenVisibility: Integer; cdecl;
+    function getName: JCharSequence; cdecl;
+    function getSound: Jnet_Uri; cdecl;
+    function getVibrationPattern: TJavaArray<Int64>; cdecl;
+    function hashCode: Integer; cdecl;
+    procedure setBypassDnd(bypassDnd: boolean); cdecl;
+    procedure setDescription(description: JString); cdecl;
+    procedure setGroup(groupId: JString); cdecl;
+    procedure setImportance(importance: Integer); cdecl;
+    procedure setLightColor(argb: Integer); cdecl;
+    procedure setLockscreenVisibility(lockscreenVisibility: Integer); cdecl;
+    procedure setName(&name: JCharSequence); cdecl;
+    procedure setShowBadge(showBadge: boolean); cdecl;
+    procedure setSound(sound: Jnet_Uri; audioAttributes: JAudioAttributes); cdecl;
+    procedure setVibrationPattern(vibrationPattern: TJavaArray<Int64>); cdecl;
+    function shouldShowLights: boolean; cdecl;
+    function shouldVibrate: boolean; cdecl;
+    function toString: JString; cdecl;
+    procedure writeToParcel(dest: JParcel; flags: Integer); cdecl;
+  end;
+  TJNotificationChannel = class(TJavaGenericImport<JNotificationChannelClass, JNotificationChannel>)
+  end;
+
+  JNotificationChannelGroupClass = interface(JObjectClass)
+    ['{E62DA0EE-18A8-439E-BE8D-A9D0A89A2EF9}']
+    function _GetCREATOR : JParcelable_Creator; cdecl;
+    function init(id : JString; &name : JCharSequence) : JNotificationChannelGroup; cdecl;
+    property CREATOR : JParcelable_Creator read _GetCREATOR;
+  end;
+
+  [JavaSignature('android/app/NotificationChannelGroup')]
+  JNotificationChannelGroup = interface(JObject)
+    ['{CA4F183A-9CD3-4261-805A-A0CCBC9A5389}']
+    function clone: JNotificationChannelGroup; cdecl;
+    function describeContents: Integer; cdecl;
+    function equals(o: JObject): boolean; cdecl;
+    function getChannels: JList; cdecl;
+    function getDescription: JString; cdecl;
+    function getId: JString; cdecl;
+    function getName: JCharSequence; cdecl;
+    function hashCode: Integer; cdecl;
+    function isBlocked: Boolean; cdecl;
+    procedure setDescription(description: JString); cdecl;
+    function toString: JString; cdecl;
+    procedure writeToParcel(dest: JParcel; flags: Integer); cdecl;
+  end;
+  TJNotificationChannelGroup = class(TJavaGenericImport<JNotificationChannelGroupClass, JNotificationChannelGroup>)
+  end;
+
+  JNotificationManagerClass = interface(JObjectClass)
+    ['{66101C50-DAE9-4C81-8186-81A0A43A73BD}']
+    {class} function _GetACTION_INTERRUPTION_FILTER_CHANGED: JString; cdecl;
+    {class} function _GetACTION_NOTIFICATION_POLICY_ACCESS_GRANTED_CHANGED: JString; cdecl;
+    {class} function _GetACTION_NOTIFICATION_POLICY_CHANGED: JString; cdecl;
+    {class} function _GetIMPORTANCE_DEFAULT: Integer; cdecl;
+    {class} function _GetIMPORTANCE_HIGH: Integer; cdecl;
+    {class} function _GetIMPORTANCE_LOW: Integer; cdecl;
+    {class} function _GetIMPORTANCE_MAX: Integer; cdecl;
+    {class} function _GetIMPORTANCE_MIN: Integer; cdecl;
+    {class} function _GetIMPORTANCE_NONE: Integer; cdecl;
+    {class} function _GetIMPORTANCE_UNSPECIFIED: Integer; cdecl;
+    {class} function _GetINTERRUPTION_FILTER_ALARMS: Integer; cdecl;
+    {class} function _GetINTERRUPTION_FILTER_ALL: Integer; cdecl;
+    {class} function _GetINTERRUPTION_FILTER_NONE: Integer; cdecl;
+    {class} function _GetINTERRUPTION_FILTER_PRIORITY: Integer; cdecl;
+    {class} function _GetINTERRUPTION_FILTER_UNKNOWN: Integer; cdecl;
+    {class} property ACTION_INTERRUPTION_FILTER_CHANGED: JString read _GetACTION_INTERRUPTION_FILTER_CHANGED;
+    {class} property ACTION_NOTIFICATION_POLICY_ACCESS_GRANTED_CHANGED: JString read _GetACTION_NOTIFICATION_POLICY_ACCESS_GRANTED_CHANGED;
+    {class} property ACTION_NOTIFICATION_POLICY_CHANGED: JString read _GetACTION_NOTIFICATION_POLICY_CHANGED;
+    {class} property IMPORTANCE_DEFAULT: Integer read _GetIMPORTANCE_DEFAULT;
+    {class} property IMPORTANCE_HIGH: Integer read _GetIMPORTANCE_HIGH;
+    {class} property IMPORTANCE_LOW: Integer read _GetIMPORTANCE_LOW;
+    {class} property IMPORTANCE_MAX: Integer read _GetIMPORTANCE_MAX;
+    {class} property IMPORTANCE_MIN: Integer read _GetIMPORTANCE_MIN;
+    {class} property IMPORTANCE_NONE: Integer read _GetIMPORTANCE_NONE;
+    {class} property IMPORTANCE_UNSPECIFIED: Integer read _GetIMPORTANCE_UNSPECIFIED;
+    {class} property INTERRUPTION_FILTER_ALARMS: Integer read _GetINTERRUPTION_FILTER_ALARMS;
+    {class} property INTERRUPTION_FILTER_ALL: Integer read _GetINTERRUPTION_FILTER_ALL;
+    {class} property INTERRUPTION_FILTER_NONE: Integer read _GetINTERRUPTION_FILTER_NONE;
+    {class} property INTERRUPTION_FILTER_PRIORITY: Integer read _GetINTERRUPTION_FILTER_PRIORITY;
+    {class} property INTERRUPTION_FILTER_UNKNOWN: Integer read _GetINTERRUPTION_FILTER_UNKNOWN;
+  end;
+
+  [JavaSignature('android/app/NotificationManager')]
+  JNotificationManager = interface(Androidapi.JNI.App.JNotificationManager)
+    ['{F2C96815-29C4-4A83-994A-4F49F30B8CF4}']
+    procedure createNotificationChannel(channel: JNotificationChannel); cdecl;
+    procedure createNotificationChannelGroup(group: JNotificationChannelGroup); cdecl;
+    procedure createNotificationChannelGroups(groups: JList); cdecl;
+    procedure createNotificationChannels(channels: JList); cdecl;
+    procedure deleteNotificationChannel(channelId: JString); cdecl;
+    procedure deleteNotificationChannelGroup(groupId: JString); cdecl;
+  end;
+  TJNotificationManager = class(TJavaGenericImport<JNotificationManagerClass, JNotificationManager>)
+  end;
+  {$ENDIF}
+
   TNotificationReceiver = class(TMultiReceiver)
   private
     FNotifications: TNotifications;
@@ -95,7 +224,10 @@ uses
   // RTL
   System.SysUtils, System.DateUtils, System.TimeSpan,
   // Android
-  Androidapi.Helpers, Androidapi.JNI.Net, Androidapi.JNI.Os, Androidapi.JNI.Embarcadero,
+  {$IF CompilerVersion < 33}
+  Androidapi.JNI.Net, Androidapi.JNI.Os,
+  {$ENDIF}
+  Androidapi.Helpers, Androidapi.JNI.Embarcadero,
   // REST
   REST.Json,
   // DW
