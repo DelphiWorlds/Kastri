dae4a168c15d4f76709db5ca2cc257940eb02fd5
Dave Nottage <davidn@radsoft.com.au>
2021-07-29 13:55:17 +0930

    Added Android Geofence feature

---
+++ Features/Geofence/DW.Geofence.Android.pas
@@ -0,0 +1,266 @@
+unit DW.Geofence.Android;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{  Copyright 2020-2021 Dave Nottage under MIT license   }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+// ************ DO NOT ADD ANY FMX UNITS (or any that depend on them) HERE ***********
+uses
+  // Android
+  Androidapi.AppGlue, Androidapi.JNIBridge, Androidapi.JNI.JavaTypes, Androidapi.JNI.GraphicsContentViewText,
+  // DW
+  DW.Geofence, DW.Androidapi.JNI.DWGeofence, DW.MultiReceiver.Android;
+
+type
+  TPlatformGeofenceManager = class;
+
+  TGeofenceTransitionReceiver = class(TMultiReceiver)
+  private
+    FManager: TPlatformGeofenceManager;
+  protected
+    procedure ConfigureActions; override;
+    procedure Receive(context: JContext; intent: JIntent); override;
+  public
+    constructor Create(const AManager: TPlatformGeofenceManager);
+  end;
+
+  TGeofenceManagerDelegate = class(TJavaLocal, JGeofenceManagerDelegate)
+  private
+    FManager: TPlatformGeofenceManager;
+  public
+    { JGeofenceManagerDelegate }
+    procedure onGeofenceActionComplete(action: Integer; result: Integer; errorMessage: JString); cdecl;
+  public
+    constructor Create(const AManager: TPlatformGeofenceManager);
+  end;
+
+  TPlatformGeofenceManager = class(TCustomPlatformGeofenceManager)
+  private
+    FDelegate: JGeofenceManagerDelegate;
+    FGeofence: JGeofenceManager;
+    FIsLoading: Boolean;
+    FIsShowingLocationSettings: Boolean;
+    FTransitionReceiver: TGeofenceTransitionReceiver;
+    FSavedApplicationCommandEventHandler: TOnApplicationCommand;
+    procedure ApplicationCommandEventHandler(const AAppGlue: TAndroidApplicationGlue; const ACommand: TAndroidApplicationCommand);
+    function GetTransitionTypes(const ATransitions: TGeofenceTransitions): Integer;
+    procedure HookApplicationCommandEvent;
+    procedure LoadRegions;
+    procedure UnhookApplicationCommandEvent;
+  protected
+    procedure DoGeofenceActionComplete(const AAction: Integer; const AResult: Integer; const AErrorMessage: string); override;
+    procedure DoGeofenceTransition(const ATransition: TGeofenceTransition; const ARegionIds: TArray<string>); override;
+    function GetRegionsJson: string; override;
+    function IsMonitoring: Boolean; override;
+    procedure RegionChange(const ARegion: TGeofenceRegion; const AChange: TRegionChange); override;
+    procedure Start; override;
+    procedure Stop; override;
+  public
+    class function GetTransition(const ATransitionType: Integer): TGeofenceTransition;
+  public
+    constructor Create(const AManager: TGeofenceManager); override;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+// ************ DO NOT ADD ANY FMX UNITS (or any that depend on them) HERE ***********
+uses
+  // RTL
+  System.SysUtils,
+  // Android
+  Androidapi.Helpers, Androidapi.NativeActivity,
+  // DW
+  DW.Android.Helpers;
+
+{ TGeofenceTransitionReceiver }
+
+constructor TGeofenceTransitionReceiver.Create(const AManager: TPlatformGeofenceManager);
+begin
+  inherited Create(True);
+  FManager := AManager;
+end;
+
+procedure TGeofenceTransitionReceiver.ConfigureActions;
+begin
+  IntentFilter.addAction(TJGeofenceIntentReceiver.JavaClass.ACTION_GEOFENCE_TRANSITION);
+end;
+
+procedure TGeofenceTransitionReceiver.Receive(context: JContext; intent: JIntent);
+var
+  LRegionIds: TArray<string>;
+  LTransition: TGeofenceTransition;
+begin
+  LTransition := TPlatformGeofenceManager.GetTransition(intent.getIntExtra(TJGeofenceIntentReceiver.JavaClass.EXTRA_TRANSITION_TYPE, 1));
+  LRegionIds :=JStringToString(intent.getStringExtra(TJGeofenceIntentReceiver.JavaClass.EXTRA_TRANSITION_IDS)).Split([',']);
+  FManager.DoGeofenceTransition(LTransition, LRegionIds);
+end;
+
+{ TGeofenceManagerDelegate }
+
+constructor TGeofenceManagerDelegate.Create(const AManager: TPlatformGeofenceManager);
+begin
+  inherited Create;
+  FManager := AManager;
+end;
+
+procedure TGeofenceManagerDelegate.onGeofenceActionComplete(action, result: Integer; errorMessage: JString);
+begin
+  FManager.DoGeofenceActionComplete(action, result, JStringToString(errorMessage));
+end;
+
+{ TPlatformGeofenceManager }
+
+constructor TPlatformGeofenceManager.Create(const AManager: TGeofenceManager);
+begin
+  inherited;
+  HookApplicationCommandEvent;
+  FTransitionReceiver := TGeofenceTransitionReceiver.Create(Self);
+  FDelegate := TGeofenceManagerDelegate.Create(Self);
+  FGeofence := TJGeofenceManager.JavaClass.init(TAndroidHelper.Context, FDelegate);
+  FIsLoading := True;
+  try
+    LoadRegions;
+  finally
+    FIsLoading := False;
+  end;
+end;
+
+destructor TPlatformGeofenceManager.Destroy;
+begin
+  FTransitionReceiver.Free;
+  UnhookApplicationCommandEvent;
+  inherited;
+end;
+
+procedure TPlatformGeofenceManager.HookApplicationCommandEvent;
+var
+  LAppGlue: TAndroidApplicationGlue;
+begin
+  if System.DelphiActivity <> nil then
+  begin
+    LAppGlue := PANativeActivity(System.DelphiActivity)^.instance;
+    FSavedApplicationCommandEventHandler := LAppGlue.OnApplicationCommandEvent;
+    LAppGlue.OnApplicationCommandEvent := ApplicationCommandEventHandler;
+  end;
+end;
+
+procedure TPlatformGeofenceManager.UnhookApplicationCommandEvent;
+var
+  LAppGlue: TAndroidApplicationGlue;
+begin
+  // Hooking App Glue here so as to not introduce any FMX units
+  if System.DelphiActivity <> nil then
+  begin
+    LAppGlue := PANativeActivity(System.DelphiActivity)^.instance;
+    LAppGlue.OnApplicationCommandEvent := FSavedApplicationCommandEventHandler;
+    FSavedApplicationCommandEventHandler := nil;
+  end;
+end;
+
+procedure TPlatformGeofenceManager.ApplicationCommandEventHandler(const AAppGlue: TAndroidApplicationGlue;
+  const ACommand: TAndroidApplicationCommand);
+begin
+  if ACommand = TAndroidApplicationCommand.Resume then
+    ApplicationBecameActive;
+  if Assigned(FSavedApplicationCommandEventHandler) then
+    FSavedApplicationCommandEventHandler(AAppGlue, ACommand);
+end;
+
+class function TPlatformGeofenceManager.GetTransition(const ATransitionType: Integer): TGeofenceTransition;
+begin
+  case ATransitionType of
+    1:
+      Result := TGeofenceTransition.RegionEnter;
+    2:
+      Result := TGeofenceTransition.RegionExit;
+  else
+    Result := TGeofenceTransition.RegionEnter;
+  end;
+end;
+
+procedure TPlatformGeofenceManager.DoGeofenceActionComplete(const AAction: Integer; const AResult: Integer; const AErrorMessage: string);
+begin
+  inherited;
+end;
+
+procedure TPlatformGeofenceManager.DoGeofenceTransition(const ATransition: TGeofenceTransition; const ARegionIds: TArray<string>);
+begin
+  inherited;
+end;
+
+function TPlatformGeofenceManager.GetRegionsJson: string;
+begin
+  Result := JStringToString(FGeofence.getRegions.toJson);
+end;
+
+function TPlatformGeofenceManager.GetTransitionTypes(const ATransitions: TGeofenceTransitions): Integer;
+begin
+  Result := 0;
+  if TGeofenceTransition.RegionEnter in ATransitions then
+    Result := Result or 1;
+  if TGeofenceTransition.RegionExit in ATransitions then
+    Result := Result or 2;
+end;
+
+function TPlatformGeofenceManager.IsMonitoring: Boolean;
+begin
+  Result := FGeofence.getIsMonitoring;
+end;
+
+procedure TPlatformGeofenceManager.LoadRegions;
+var
+  LIterator: JIterator;
+  LJRegion: JGeofenceRegions_Region;
+  LRegion: TGeofenceRegion;
+begin
+  LIterator := FGeofence.getRegions.getItems.entrySet.iterator;
+  while LIterator.hasNext do
+  begin
+    LJRegion := TJGeofenceRegions_Region.Wrap(LIterator.next);
+    LRegion.ID := JStringToString(LJRegion.getId);
+    LRegion.Coords.Latitude := LJRegion.getCoords.latitude;
+    LRegion.Coords.Longitude := LJRegion.getCoords.longitude;
+    LRegion.Radius := LJRegion.getRadius;
+    Regions.Add(LRegion);
+  end;
+end;
+
+procedure TPlatformGeofenceManager.RegionChange(const ARegion: TGeofenceRegion; const AChange: TRegionChange);
+begin
+  if not FIsLoading then
+  begin
+    case AChange of
+      TRegionChange.Add:
+      begin
+        FGeofence.getRegions.add(StringToJString(ARegion.ID), ARegion.Coords.Latitude, ARegion.Coords.Longitude, ARegion.Radius,
+          GetTransitionTypes(ARegion.Transitions));
+      end;
+      TRegionChange.Remove:
+        FGeofence.getRegions.remove(StringToJString(ARegion.ID));
+    end;
+  end;
+end;
+
+procedure TPlatformGeofenceManager.Start;
+begin
+  FGeofence.start;
+end;
+
+procedure TPlatformGeofenceManager.Stop;
+begin
+  FGeofence.stop;
+end;
+
+end.
---
+++ Features/Geofence/DW.Geofence.pas
@@ -0,0 +1,371 @@
+unit DW.Geofence;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{  Copyright 2020-2021 Dave Nottage under MIT license   }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+// ************ DO NOT ADD ANY FMX UNITS (or any that depend on them) HERE ***********
+uses
+  // RTL
+  System.Classes, System.Sensors, System.Generics.Collections, System.SysUtils;
+
+type
+  TGeofenceTransition = (RegionEnter, RegionExit);
+
+  TGeofenceTransitions = set of TGeofenceTransition;
+
+const
+  cTransitionsAll: TGeofenceTransitions = [TGeofenceTransition.RegionEnter, TGeofenceTransition.RegionExit];
+
+type
+  TGeofenceRegion = record
+    ID: string;
+    Coords: TLocationCoord2D;
+    Radius: Double;
+    Transitions: TGeofenceTransitions;
+  end;
+
+  TCustomPlatformGeofenceManager = class;
+
+  TGeofenceRegions = class(TList<TGeofenceRegion>)
+  private
+    FManager: TCustomPlatformGeofenceManager;
+    function IndexOfID(const AID: string): Integer;
+  protected
+    procedure Notify(const Item: TGeofenceRegion; Action: TCollectionNotification); override;
+  public
+    constructor Create(const AManager: TCustomPlatformGeofenceManager);
+    function AddRegion(const AID: string; const ALatitude: Double; const ALongitude: Double; const ARadius: Double): Integer; overload;
+    function AddRegion(const AID: string; const ALatitude: Double; const ALongitude: Double; const ARadius: Double;
+      const ATransitions: TGeofenceTransitions): Integer; overload;
+    procedure RemoveRegion(const AID: string);
+    function ToJson: string;
+  end;
+
+  TGeofenceManager = class;
+
+  TRegionChange = (Add, Remove);
+
+  TCustomPlatformGeofenceManager = class(TObject)
+  private
+    FManager: TGeofenceManager;
+    FRegions: TGeofenceRegions;
+  protected
+    procedure ApplicationBecameActive; virtual;
+    procedure DoGeofenceActionComplete(const AAction: Integer; const AResult: Integer; const AErrorMessage: string); virtual;
+    procedure DoGeofenceTransition(const ATransition: TGeofenceTransition; const ARegionIds: TArray<string>); virtual;
+    function GetRegionsJson: string; virtual;
+    function IsMonitoring: Boolean; virtual;
+    procedure RegionChange(const ARegion: TGeofenceRegion; const AChange: TRegionChange); virtual;
+    procedure Start; virtual;
+    procedure Stop; virtual;
+    property Regions: TGeofenceRegions read FRegions;
+  public
+    constructor Create(const AManager: TGeofenceManager); virtual;
+    destructor Destroy; override;
+  end;
+
+  TGeofenceActionCompleteEvent = procedure(Sender: TObject; const Action: Integer; const Result: Integer; const ErrorMessage: string) of object;
+  TGeofenceTransitionEvent = procedure(Sender: TObject; const Transition: TGeofenceTransition; const RegionIds: TArray<string>) of object;
+  TShowPermissionsEvent = procedure(Sender: TObject; const CompletionHandler: TProc) of object;
+
+  TGeofenceManager = class(TObject)
+  private
+    FIsShowingAppSettings: Boolean;
+    FPlatformManager: TCustomPlatformGeofenceManager;
+    FOnGeofenceActionComplete: TGeofenceActionCompleteEvent;
+    FOnGeofenceTransition: TGeofenceTransitionEvent;
+    FOnPermissionsResult: TNotifyEvent;
+    FOnShowBackgroundPermissions: TShowPermissionsEvent;
+    procedure DoStart;
+    function GetRegions: TGeofenceRegions;
+    function GetIsMonitoring: Boolean;
+    procedure DoPermissionsResult;
+    procedure DoRequestBackgroundLocationPermission;
+    procedure RequestBackgroundLocationPermission;
+    procedure RequestForegroundLocationPermission;
+    procedure RequestPermissions;
+  protected
+    procedure ApplicationBecameActive;
+    procedure DoGeofenceActionComplete(const AAction: Integer; const AResult: Integer; const AErrorMessage: string);
+    procedure DoGeofenceTransition(const ATransition: TGeofenceTransition; const ARegionIds: TArray<string>);
+  public
+    constructor Create;
+    function HasPermissions: Boolean;
+    procedure ShowAppSettings;
+    procedure Start;
+    procedure Stop;
+    property IsMonitoring: Boolean read GetIsMonitoring;
+    property Regions: TGeofenceRegions read GetRegions;
+    property OnGeofenceActionComplete: TGeofenceActionCompleteEvent read FOnGeofenceActionComplete write FOnGeofenceActionComplete;
+    property OnGeofenceTransition: TGeofenceTransitionEvent read FOnGeofenceTransition write FOnGeofenceTransition;
+    property OnPermissionsResult: TNotifyEvent read FOnPermissionsResult write FOnPermissionsResult;
+    property OnShowBackgroundPermissions: TShowPermissionsEvent read FOnShowBackgroundPermissions write FOnShowBackgroundPermissions;
+  end;
+
+implementation
+
+// ************ DO NOT ADD ANY FMX UNITS (or any that depend on them) HERE ***********
+uses
+  // RTL
+  System.Permissions,
+  // DW
+{$IF Defined(ANDROID)}
+  DW.Geofence.Android,
+{$ENDIF}
+  DW.Consts.Android, DW.Permissions.Helpers, DW.OSDevice;
+
+{$IF not Defined(ANDROID)}
+type
+  TPlatformGeofenceManager = class(TCustomPlatformGeofenceManager);
+{$ENDIF}
+
+{ TGeofenceRegions }
+
+constructor TGeofenceRegions.Create(const AManager: TCustomPlatformGeofenceManager);
+begin
+  inherited Create;
+  FManager := AManager;
+end;
+
+function TGeofenceRegions.IndexOfID(const AID: string): Integer;
+var
+  I: Integer;
+begin
+  Result := -1;
+  for I := 0 to Count - 1 do
+  begin
+    if Items[I].ID.Equals(AID) then
+    begin
+      Result := I;
+      Break;
+    end;
+  end;
+end;
+
+function TGeofenceRegions.AddRegion(const AID: string; const ALatitude, ALongitude, ARadius: Double): Integer;
+begin
+  Result := AddRegion(AID, ALatitude, ALongitude, ARadius, cTransitionsAll);
+end;
+
+function TGeofenceRegions.AddRegion(const AID: string; const ALatitude, ALongitude, ARadius: Double;
+  const ATransitions: TGeofenceTransitions): Integer;
+var
+  LIndex: Integer;
+  LRegion: TGeofenceRegion;
+begin
+  LIndex := IndexOfID(AID);
+  if LIndex > -1 then
+    Delete(LIndex);
+  LRegion.ID := AID;
+  LRegion.Coords := TLocationCoord2D.Create(ALatitude, ALongitude);
+  LRegion.Radius := ARadius;
+  LRegion.Transitions := ATransitions;
+  Result := Add(LRegion);
+end;
+
+procedure TGeofenceRegions.Notify(const Item: TGeofenceRegion; Action: TCollectionNotification);
+begin
+  inherited;
+  case Action of
+    TCollectionNotification.cnAdded:
+      FManager.RegionChange(Item, TRegionChange.Add);
+    TCollectionNotification.cnRemoved:
+      FManager.RegionChange(Item, TRegionChange.Remove);
+  end;
+end;
+
+procedure TGeofenceRegions.RemoveRegion(const AID: string);
+var
+  LIndex: Integer;
+begin
+  LIndex := IndexOfID(AID);
+  if LIndex > -1 then
+    Delete(LIndex);
+end;
+
+function TGeofenceRegions.ToJson: string;
+begin
+  Result := FManager.GetRegionsJson;
+end;
+
+{ TCustomPlatformGeofenceManager }
+
+constructor TCustomPlatformGeofenceManager.Create(const AManager: TGeofenceManager);
+begin
+  inherited Create;
+  FManager := AManager;
+  FRegions := TGeofenceRegions.Create(Self);
+end;
+
+destructor TCustomPlatformGeofenceManager.Destroy;
+begin
+  FRegions.Free;
+  inherited;
+end;
+
+procedure TCustomPlatformGeofenceManager.ApplicationBecameActive;
+begin
+  FManager.ApplicationBecameActive;
+end;
+
+procedure TCustomPlatformGeofenceManager.DoGeofenceActionComplete(const AAction, AResult: Integer; const AErrorMessage: string);
+begin
+  FManager.DoGeofenceActionComplete(AAction, AResult, AErrorMessage);
+end;
+
+procedure TCustomPlatformGeofenceManager.DoGeofenceTransition(const ATransition: TGeofenceTransition; const ARegionIds: TArray<string>);
+begin
+  FManager.DoGeofenceTransition(ATransition, ARegionIds);
+end;
+
+function TCustomPlatformGeofenceManager.GetRegionsJson: string;
+begin
+  Result := '';
+end;
+
+function TCustomPlatformGeofenceManager.IsMonitoring: Boolean;
+begin
+  Result := False;
+end;
+
+procedure TCustomPlatformGeofenceManager.RegionChange(const ARegion: TGeofenceRegion; const AChange: TRegionChange);
+begin
+  //
+end;
+
+procedure TCustomPlatformGeofenceManager.Start;
+begin
+  //
+end;
+
+procedure TCustomPlatformGeofenceManager.Stop;
+begin
+  //
+end;
+
+{ TGeofenceManager }
+
+constructor TGeofenceManager.Create;
+begin
+  inherited Create;
+  FPlatformManager := TPlatformGeofenceManager.Create(Self);
+end;
+
+procedure TGeofenceManager.DoGeofenceActionComplete(const AAction, AResult: Integer; const AErrorMessage: string);
+begin
+  if Assigned(FOnGeofenceActionComplete) then
+    FOnGeofenceActionComplete(Self, AAction, AResult, AErrorMessage);
+end;
+
+procedure TGeofenceManager.DoGeofenceTransition(const ATransition: TGeofenceTransition; const ARegionIds: TArray<string>);
+begin
+  if Assigned(FOnGeofenceTransition) then
+    FOnGeofenceTransition(Self, ATransition, ARegionIds);
+end;
+
+function TGeofenceManager.GetIsMonitoring: Boolean;
+begin
+  Result := FPlatformManager.IsMonitoring;
+end;
+
+function TGeofenceManager.GetRegions: TGeofenceRegions;
+begin
+  Result := FPlatformManager.Regions;
+end;
+
+function TGeofenceManager.HasPermissions: Boolean;
+begin
+  Result := PermissionsService.IsEveryPermissionGranted([cPermissionAccessFineLocation, cPermissionAccessBackgroundLocation]);
+end;
+
+procedure TGeofenceManager.DoPermissionsResult;
+begin
+  if Assigned(FOnPermissionsResult) then
+    FOnPermissionsResult(Self);
+end;
+
+procedure TGeofenceManager.DoRequestBackgroundLocationPermission;
+begin
+  PermissionsService.RequestPermissions([cPermissionAccessBackgroundLocation],
+    procedure(const APermissions: TArray<string>; const AGrantResults: TArray<TPermissionStatus>)
+    begin
+      if AGrantResults.AreAllGranted then
+        DoStart;
+      DoPermissionsResult;
+    end
+  );
+end;
+
+procedure TGeofenceManager.RequestBackgroundLocationPermission;
+begin
+  if Assigned(FOnShowBackgroundPermissions) then
+    FOnShowBackgroundPermissions(Self, DoRequestBackgroundLocationPermission)
+  else
+    DoRequestBackgroundLocationPermission;
+end;
+
+procedure TGeofenceManager.RequestForegroundLocationPermission;
+begin
+  PermissionsService.RequestPermissions([cPermissionAccessFineLocation],
+    procedure(const APermissions: TArray<string>; const AGrantResults: TArray<TPermissionStatus>)
+    begin
+      if AGrantResults.AreAllGranted then
+        RequestBackgroundLocationPermission
+      else
+        DoPermissionsResult;
+    end
+  );
+end;
+
+procedure TGeofenceManager.RequestPermissions;
+begin
+  if PermissionsService.IsPermissionGranted(cPermissionAccessFineLocation) then
+    RequestBackgroundLocationPermission
+  else
+    RequestForegroundLocationPermission;
+end;
+
+procedure TGeofenceManager.ShowAppSettings;
+begin
+  TOSDevice.OpenAppSettings;
+  FIsShowingAppSettings := True;
+end;
+
+procedure TGeofenceManager.ApplicationBecameActive;
+begin
+  if FIsShowingAppSettings and HasPermissions and not IsMonitoring then
+    DoStart;
+  FIsShowingAppSettings := False;
+end;
+
+procedure TGeofenceManager.Start;
+begin
+  if HasPermissions then
+    DoStart
+  else
+    RequestPermissions;
+end;
+
+procedure TGeofenceManager.DoStart;
+begin
+  FPlatformManager.Start;
+end;
+
+procedure TGeofenceManager.Stop;
+begin
+  FPlatformManager.Stop;
+end;
+
+end.
