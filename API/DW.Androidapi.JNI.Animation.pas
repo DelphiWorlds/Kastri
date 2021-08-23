eb1da8530dd990089b48062855188f7c04bfc9b7
Dave Nottage <davidn@radsoft.com.au>
2021-08-23 14:50:56 +0930

    Added import for some animation classes

---
+++ API/DW.Androidapi.JNI.Animation.pas
@@ -0,0 +1,326 @@
+unit DW.Androidapi.JNI.Animation;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{  Copyright 2020-2021 Dave Nottage under MIT license   }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+uses
+  // Android
+  Androidapi.JNIBridge, Androidapi.JNI.GraphicsContentViewText, Androidapi.JNI.JavaTypes, Androidapi.JNI.Util;
+
+type
+  JAnimator = interface;
+  JAnimator_AnimatorListener = interface;
+  JAnimator_AnimatorPauseListener = interface;
+  JKeyframe = interface;
+  JLayoutTransition = interface;
+  JLayoutTransition_TransitionListener = interface;
+  JPropertyValuesHolder = interface;
+  JStateListAnimator = interface;
+  JTimeInterpolator = interface;
+  JTypeConverter = interface;
+  JTypeEvaluator = interface;
+  JValueAnimator = interface;
+  JValueAnimator_AnimatorUpdateListener = interface;
+
+  JAnimatorClass = interface(JObjectClass)
+    ['{D187B9AD-6A9F-44A7-8DD2-280F7482BEC4}']
+    {class} function init: JAnimator; cdecl;
+  end;
+
+  [JavaSignature('android/animation/Animator')]
+  JAnimator = interface(JObject)
+    ['{C23DB294-45C3-4FD3-AD52-ED9DF291CD4C}']
+    procedure addListener(listener: JAnimator_AnimatorListener); cdecl;
+    procedure addPauseListener(listener: JAnimator_AnimatorPauseListener); cdecl;
+    procedure cancel; cdecl;
+    function clone: JAnimator; cdecl;
+    procedure &end; cdecl;
+    function getDuration: Int64; cdecl;
+    function getInterpolator: JTimeInterpolator; cdecl;
+    function getListeners: JArrayList; cdecl;
+    function getStartDelay: Int64; cdecl;
+    function isPaused: Boolean; cdecl;
+    function isRunning: Boolean; cdecl;
+    function isStarted: Boolean; cdecl;
+    procedure pause; cdecl;
+    procedure removeAllListeners; cdecl;
+    procedure removeListener(listener: JAnimator_AnimatorListener); cdecl;
+    procedure removePauseListener(listener: JAnimator_AnimatorPauseListener); cdecl;
+    procedure resume; cdecl;
+    function setDuration(duration: Int64): JAnimator; cdecl;
+    procedure setInterpolator(value: JTimeInterpolator); cdecl;
+    procedure setStartDelay(startDelay: Int64); cdecl;
+    procedure setupEndValues; cdecl;
+    procedure setupStartValues; cdecl;
+    procedure setTarget(target: JObject); cdecl;
+    procedure start; cdecl;
+  end;
+  TJAnimator = class(TJavaGenericImport<JAnimatorClass, JAnimator>) end;
+
+  JAnimator_AnimatorListenerClass = interface(IJavaClass)
+    ['{AFBC29AA-0704-483B-A9D1-BA389A69FEC4}']
+  end;
+
+  [JavaSignature('android/animation/Animator$AnimatorListener')]
+  JAnimator_AnimatorListener = interface(IJavaInstance)
+    ['{F8AE784C-CD2A-44CB-9457-EA7C991604A3}']
+    procedure onAnimationCancel(animation: JAnimator); cdecl;
+    procedure onAnimationEnd(animation: JAnimator); cdecl;
+    procedure onAnimationRepeat(animation: JAnimator); cdecl;
+    procedure onAnimationStart(animation: JAnimator); cdecl;
+  end;
+  TJAnimator_AnimatorListener = class(TJavaGenericImport<JAnimator_AnimatorListenerClass, JAnimator_AnimatorListener>) end;
+
+  JAnimator_AnimatorPauseListenerClass = interface(IJavaClass)
+    ['{952B95A0-9023-4556-ADC7-511CAD9D7F11}']
+  end;
+
+  [JavaSignature('android/animation/Animator$AnimatorPauseListener')]
+  JAnimator_AnimatorPauseListener = interface(IJavaInstance)
+    ['{8032C6CD-7C31-4785-ADF9-E8EABB571956}']
+    procedure onAnimationPause(animation: JAnimator); cdecl;
+    procedure onAnimationResume(animation: JAnimator); cdecl;
+  end;
+  TJAnimator_AnimatorPauseListener = class(TJavaGenericImport<JAnimator_AnimatorPauseListenerClass, JAnimator_AnimatorPauseListener>) end;
+
+  JKeyframeClass = interface(JObjectClass)
+    ['{9B9A839D-CC4D-4961-9435-FC69403F6E73}']
+    {class} function init: JKeyframe; cdecl;
+    {class} function ofFloat(fraction: Single; value: Single): JKeyframe; cdecl; overload;
+    {class} function ofFloat(fraction: Single): JKeyframe; cdecl; overload;
+    {class} function ofInt(fraction: Single; value: Integer): JKeyframe; cdecl; overload;
+    {class} function ofInt(fraction: Single): JKeyframe; cdecl; overload;
+    {class} function ofObject(fraction: Single; value: JObject): JKeyframe; cdecl; overload;
+    {class} function ofObject(fraction: Single): JKeyframe; cdecl; overload;
+  end;
+
+  [JavaSignature('android/animation/Keyframe')]
+  JKeyframe = interface(JObject)
+    ['{01B85B75-BBBD-4F4F-8D53-80615B4CE0BF}']
+    function clone: JKeyframe; cdecl;
+    function getFraction: Single; cdecl;
+    function getInterpolator: JTimeInterpolator; cdecl;
+    function getType: Jlang_Class; cdecl;
+    function getValue: JObject; cdecl;
+    function hasValue: Boolean; cdecl;
+    procedure setFraction(fraction: Single); cdecl;
+    procedure setInterpolator(interpolator: JTimeInterpolator); cdecl;
+    procedure setValue(value: JObject); cdecl;
+  end;
+  TJKeyframe = class(TJavaGenericImport<JKeyframeClass, JKeyframe>) end;
+
+  JLayoutTransitionClass = interface(JObjectClass)
+    ['{ED6AEDD6-F55B-4FB2-8676-91C4B8212266}']
+    {class} function _GetAPPEARING: Integer; cdecl;
+    {class} function _GetCHANGE_APPEARING: Integer; cdecl;
+    {class} function _GetCHANGE_DISAPPEARING: Integer; cdecl;
+    {class} function _GetCHANGING: Integer; cdecl;
+    {class} function _GetDISAPPEARING: Integer; cdecl;
+    {class} function init: JLayoutTransition; cdecl;
+    {class} property APPEARING: Integer read _GetAPPEARING;
+    {class} property CHANGE_APPEARING: Integer read _GetCHANGE_APPEARING;
+    {class} property CHANGE_DISAPPEARING: Integer read _GetCHANGE_DISAPPEARING;
+    {class} property CHANGING: Integer read _GetCHANGING;
+    {class} property DISAPPEARING: Integer read _GetDISAPPEARING;
+  end;
+
+  [JavaSignature('android/animation/LayoutTransition')]
+  JLayoutTransition = interface(JObject)
+    ['{BDCFC2F3-79FD-4F73-9B28-E64C0140F4E7}']
+    procedure addChild(parent: JViewGroup; child: JView); cdecl;
+    procedure addTransitionListener(listener: JLayoutTransition_TransitionListener); cdecl;
+    procedure disableTransitionType(transitionType: Integer); cdecl;
+    procedure enableTransitionType(transitionType: Integer); cdecl;
+    function getAnimator(transitionType: Integer): JAnimator; cdecl;
+    function getDuration(transitionType: Integer): Int64; cdecl;
+    function getInterpolator(transitionType: Integer): JTimeInterpolator; cdecl;
+    function getStagger(transitionType: Integer): Int64; cdecl;
+    function getStartDelay(transitionType: Integer): Int64; cdecl;
+    function getTransitionListeners: JList; cdecl;
+    procedure hideChild(parent: JViewGroup; child: JView); cdecl; overload;
+    procedure hideChild(parent: JViewGroup; child: JView; newVisibility: Integer); cdecl; overload;
+    function isChangingLayout: Boolean; cdecl;
+    function isRunning: Boolean; cdecl;
+    function isTransitionTypeEnabled(transitionType: Integer): Boolean; cdecl;
+    procedure removeChild(parent: JViewGroup; child: JView); cdecl;
+    procedure removeTransitionListener(listener: JLayoutTransition_TransitionListener); cdecl;
+    procedure setAnimateParentHierarchy(animateParentHierarchy: Boolean); cdecl;
+    procedure setAnimator(transitionType: Integer; animator: JAnimator); cdecl;
+    procedure setDuration(duration: Int64); cdecl; overload;
+    procedure setDuration(transitionType: Integer; duration: Int64); cdecl; overload;
+    procedure setInterpolator(transitionType: Integer; interpolator: JTimeInterpolator); cdecl;
+    procedure setStagger(transitionType: Integer; duration: Int64); cdecl;
+    procedure setStartDelay(transitionType: Integer; delay: Int64); cdecl;
+    procedure showChild(parent: JViewGroup; child: JView); cdecl; overload;
+    procedure showChild(parent: JViewGroup; child: JView; oldVisibility: Integer); cdecl; overload;
+  end;
+  TJLayoutTransition = class(TJavaGenericImport<JLayoutTransitionClass, JLayoutTransition>) end;
+
+  JLayoutTransition_TransitionListenerClass = interface(IJavaClass)
+    ['{7D486CC1-68C7-4744-9ABE-49011789D218}']
+  end;
+
+  [JavaSignature('android/animation/LayoutTransition$TransitionListener')]
+  JLayoutTransition_TransitionListener = interface(IJavaInstance)
+    ['{3E6D9947-DA32-4C35-82E6-7FD2884007A0}']
+    procedure startTransition(transition: JLayoutTransition; container: JViewGroup; view: JView; transitionType: Integer); cdecl;
+    procedure endTransition(transition: JLayoutTransition; container: JViewGroup; view: JView; transitionType: Integer); cdecl;
+  end;
+  TJLayoutTransition_TransitionListener = class(TJavaGenericImport<JLayoutTransition_TransitionListenerClass,
+    JLayoutTransition_TransitionListener>) end;
+
+  JPropertyValuesHolderClass = interface(JObjectClass)
+    ['{0F89C9AE-2947-4FC5-B5E2-FB36935AA11A}']
+    {class} function ofMultiFloat(propertyName: JString; values: TJavaBiArray<Single>): JPropertyValuesHolder; cdecl; overload;
+    {class} function ofMultiFloat(propertyName: JString; path: JPath): JPropertyValuesHolder; cdecl; overload;
+    {class} function ofMultiInt(propertyName: JString; values: TJavaBiArray<Integer>): JPropertyValuesHolder; cdecl; overload;
+    {class} function ofMultiInt(propertyName: JString; path: JPath): JPropertyValuesHolder; cdecl; overload;
+    {class} function ofObject(propertyName: JString; converter: JTypeConverter; path: JPath): JPropertyValuesHolder; cdecl; overload;
+    {class} function ofObject(property_: JProperty; converter: JTypeConverter; path: JPath): JPropertyValuesHolder; cdecl; overload;
+  end;
+
+  [JavaSignature('android/animation/PropertyValuesHolder')]
+  JPropertyValuesHolder = interface(JObject)
+    ['{6422ED7A-C75F-4632-9962-E582B11BBECB}']
+    function clone: JPropertyValuesHolder; cdecl;
+    function getPropertyName: JString; cdecl;
+    procedure setConverter(converter: JTypeConverter); cdecl;
+    procedure setEvaluator(evaluator: JTypeEvaluator); cdecl;
+    procedure setProperty(property_: JProperty); cdecl;
+    procedure setPropertyName(propertyName: JString); cdecl;
+    function toString: JString; cdecl;
+  end;
+  TJPropertyValuesHolder = class(TJavaGenericImport<JPropertyValuesHolderClass, JPropertyValuesHolder>) end;
+
+  JStateListAnimatorClass = interface(JObjectClass)
+    ['{28E3B107-CA3D-470C-B461-877551215EA6}']
+    {class} function init: JStateListAnimator; cdecl;
+  end;
+
+  [JavaSignature('android/animation/StateListAnimator')]
+  JStateListAnimator = interface(JObject)
+    ['{81BB367C-723A-44C9-A4BE-67888DAE228C}']
+    procedure addState(specs: TJavaArray<Integer>; animator: JAnimator); cdecl;
+    function clone: JStateListAnimator; cdecl;
+    procedure jumpToCurrentState; cdecl;
+  end;
+  TJStateListAnimator = class(TJavaGenericImport<JStateListAnimatorClass, JStateListAnimator>) end;
+
+  JTimeInterpolatorClass = interface(IJavaClass)
+    ['{53135608-E880-476B-9B27-FC3D4DDA9A29}']
+  end;
+
+  [JavaSignature('android/animation/TimeInterpolator')]
+  JTimeInterpolator = interface(IJavaInstance)
+    ['{E3258923-BB71-4925-9951-E1D7B231E409}']
+    function getInterpolation(input: Single): Single; cdecl;
+  end;
+  TJTimeInterpolator = class(TJavaGenericImport<JTimeInterpolatorClass, JTimeInterpolator>) end;
+
+  JTypeConverterClass = interface(JObjectClass)
+    ['{299354DB-0C82-4759-BD9A-EFA5A000B0CA}']
+    {class} function init(fromClass: Jlang_Class; toClass: Jlang_Class): JTypeConverter; cdecl;
+  end;
+
+  [JavaSignature('android/animation/TypeConverter')]
+  JTypeConverter = interface(JObject)
+    ['{997EB1FC-EF99-48B0-8D95-19EFE1E44125}']
+    function convert(value: JObject): JObject; cdecl;
+  end;
+  TJTypeConverter = class(TJavaGenericImport<JTypeConverterClass, JTypeConverter>) end;
+
+  JTypeEvaluatorClass = interface(IJavaClass)
+    ['{4DD60E83-55BE-40C1-8A31-20289F01A893}']
+  end;
+
+  [JavaSignature('android/animation/TypeEvaluator')]
+  JTypeEvaluator = interface(IJavaInstance)
+    ['{D9BC0242-138D-4D53-82A3-65D141FA512F}']
+    function evaluate(fraction: Single; startValue: JObject; endValue: JObject): JObject; cdecl;
+  end;
+  TJTypeEvaluator = class(TJavaGenericImport<JTypeEvaluatorClass, JTypeEvaluator>) end;
+
+  JValueAnimatorClass = interface(JAnimatorClass)
+    ['{7BDC4D65-4501-4080-8E94-0E898C50FE52}']
+    {class} function _GetINFINITE: Integer; cdecl;
+    {class} function _GetRESTART: Integer; cdecl;
+    {class} function _GetREVERSE: Integer; cdecl;
+    {class} function areAnimatorsEnabled: Boolean; cdecl; // API 26
+    {class} function init: JValueAnimator; cdecl;
+    {class} property INFINITE: Integer read _GetINFINITE;
+    {class} property RESTART: Integer read _GetRESTART;
+    {class} property REVERSE: Integer read _GetREVERSE;
+  end;
+
+  [JavaSignature('android/animation/ValueAnimator')]
+  JValueAnimator = interface(JAnimator)
+    ['{8EC65DBE-CABB-4CEE-8245-DFED4ECF1FD3}']
+    procedure addUpdateListener(listener: JValueAnimator_AnimatorUpdateListener); cdecl;
+    procedure cancel; cdecl;
+    function clone: JValueAnimator; cdecl;
+    procedure &end; cdecl;
+    function getAnimatedFraction: Single; cdecl;
+    function getAnimatedValue: JObject; cdecl; overload;
+    function getAnimatedValue(propertyName: JString): JObject; cdecl; overload;
+    function getCurrentPlayTime: Int64; cdecl;
+    function getDuration: Int64; cdecl;
+    function getFrameDelay: Int64; cdecl;
+    function getInterpolator: JTimeInterpolator; cdecl;
+    function getRepeatCount: Integer; cdecl;
+    function getRepeatMode: Integer; cdecl;
+    function getStartDelay: Int64; cdecl;
+    function getValues: TJavaObjectArray<JPropertyValuesHolder>; cdecl;
+    function isRunning: Boolean; cdecl;
+    function isStarted: Boolean; cdecl;
+    procedure pause; cdecl;
+    procedure removeAllUpdateListeners; cdecl;
+    procedure removeUpdateListener(listener: JValueAnimator_AnimatorUpdateListener); cdecl;
+    procedure resume; cdecl;
+    procedure reverse; cdecl;
+    procedure setCurrentFraction(fraction: Single); cdecl;
+    procedure setCurrentPlayTime(playTime: Int64); cdecl;
+    function setDuration(duration: Int64): JValueAnimator; cdecl;
+    procedure setEvaluator(value: JTypeEvaluator); cdecl;
+    procedure setInterpolator(value: JTimeInterpolator); cdecl;
+    procedure setFrameDelay(frameDelay: Int64); cdecl;
+    procedure setRepeatCount(value: Integer); cdecl;
+    procedure setRepeatMode(value: Integer); cdecl;
+    procedure setStartDelay(startDelay: Int64); cdecl;
+    procedure start; cdecl;
+    function toString: JString; cdecl;
+  end;
+  TJValueAnimator = class(TJavaGenericImport<JValueAnimatorClass, JValueAnimator>) end;
+
+  JValueAnimator_AnimatorUpdateListenerClass = interface(IJavaClass)
+    ['{F3915100-181E-4E76-B8BC-4F452D2D4E32}']
+  end;
+
+  [JavaSignature('android/animation/ValueAnimator$AnimatorUpdateListener')]
+  JValueAnimator_AnimatorUpdateListener = interface(IJavaInstance)
+    ['{59D9F287-68A8-47BA-8790-B87EEF018BAE}']
+    procedure onAnimationUpdate(animation: JValueAnimator); cdecl;
+  end;
+  TJValueAnimator_AnimatorUpdateListener = class(TJavaGenericImport<JValueAnimator_AnimatorUpdateListenerClass,
+    JValueAnimator_AnimatorUpdateListener>) end;
+
+  JViewAnimationUtilsClass = interface(JObjectClass)
+    ['{01DD08A6-96FC-42A9-92C1-BE891FB76092}']
+    {class} function createCircularReveal(view: JView; i: Integer; i1: Integer; f: Single; f1: Single): JAnimator; cdecl;
+  end;
+
+implementation
+
+end.
+
