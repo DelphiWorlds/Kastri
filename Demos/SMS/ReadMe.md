768e4fddf0b4cceca3762848410b89b4210e8231
Dave Nottage <davidn@radsoft.com.au>
2021-11-03 09:48:20 +1030

    Added SMS demo

---
+++ Demos/SMS/ReadMe.md
@@ -0,0 +1,20 @@
+# SMS Demo
+
+## Description
+
+Demonstrates the use of the SMS feature in Kastri
+
+## Supported Delphi versions
+
+The demos should compile and work for at least versions 10.4.x and 11 - please note that you need to use SMSDemoD11 for Delphi 11 and above
+
+## Supported Platforms
+
+Supported platforms are iOS and Android
+
+## Test numbers
+
+The test numbers in the demo form are Australian, and messages sent to them can be accessed [here](https://receive-sms.cc/Australia-Phone-Number/)
+
+This service has numbers available for other countries as well
+
---
+++ Demos/SMS/Unit1.pas
@@ -0,0 +1,71 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types, FMX.ScrollBox, FMX.Memo, FMX.Controls.Presentation, FMX.StdCtrls,
+  DW.SMS;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    RecipientsMemo: TMemo;
+    Label2: TLabel;
+    TextMemo: TMemo;
+    SendButton: TButton;
+    MessagesMemo: TMemo;
+    Label3: TLabel;
+    procedure SendButtonClick(Sender: TObject);
+  private
+    FSMS: TSMS;
+    procedure SMSPermissionRequestResultHandler(Sender: TObject; const AIsGranted: Boolean);
+    procedure SMSMessageResultHandler(Sender: TObject; const ADestinations: TArray<string>; const AMessageResult: TMessageResult);
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+{ TForm1 }
+
+constructor TForm1.Create(AOwner: TComponent);
+begin
+  inherited;
+  SendButton.Enabled := False;
+  FSMS := TSMS.Create;
+  FSMS.OnMessageResult := SMSMessageResultHandler;
+  FSMS.OnPermissionRequestResult := SMSPermissionRequestResultHandler;
+  FSMS.RequestPermission;
+end;
+
+destructor TForm1.Destroy;
+begin
+  FSMS.Free;
+  inherited;
+end;
+
+procedure TForm1.SMSMessageResultHandler(Sender: TObject; const ADestinations: TArray<string>; const AMessageResult: TMessageResult);
+const
+  cMessageResultCaptions: array[TMessageResult] of string = ('cancelled', 'failed', 'sent');
+begin
+  MessagesMemo.Lines.Add(Format('Message to %s: %s', [string.Join(', ', ADestinations), cMessageResultCaptions[AMessageResult]]));
+end;
+
+procedure TForm1.SMSPermissionRequestResultHandler(Sender: TObject; const AIsGranted: Boolean);
+begin
+  SendButton.Enabled := AIsGranted;
+end;
+
+procedure TForm1.SendButtonClick(Sender: TObject);
+begin
+  FSMS.SendTextMessage(TextMemo.Text, RecipientsMemo.Lines.ToStringArray);
+end;
+
+end.
