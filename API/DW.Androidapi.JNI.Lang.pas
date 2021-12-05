c08c072100c1f6a0b9e1cf0961dea85a6629c6ce
Dave Nottage <davidn@radsoft.com.au>
2021-12-06 06:30:15 +1030

    Rename Jlang_Process

--- API/DW.Androidapi.JNI.Lang.pas
+++ API/DW.Androidapi.JNI.Lang.pas
@@ -20,16 +20,16 @@ uses
   Androidapi.JNIBridge, Androidapi.JNI.JavaTypes;
 
 type
-  Jlang_Process = interface;
+  JProcess = interface;
   JRuntime = interface;
 
-  Jlang_ProcessClass = interface(JObjectClass)
+  JProcessClass = interface(JObjectClass)
     ['{F57F6F1A-B4A2-4CD0-9ACB-06CBEBDCA561}']
-    {class} function init: Jlang_Process; cdecl;
+    {class} function init: JProcess; cdecl;
   end;
 
   [JavaSignature('java/lang/Process')]
-  Jlang_Process = interface(JObject)
+  JProcess = interface(JObject)
     ['{1B505DDF-5BE4-40C8-BC81-75AAA5158B02}']
     procedure destroy; cdecl;
     function exitValue: Integer; cdecl;
@@ -38,7 +38,7 @@ type
     function getOutputStream: JOutputStream; cdecl;
     function waitFor: Integer; cdecl;
   end;
-  TJlang_Process = class(TJavaGenericImport<Jlang_ProcessClass, Jlang_Process>) end;
+  TJProcess = class(TJavaGenericImport<JProcessClass, JProcess>) end;
 
   JRuntimeClass = interface(JObjectClass)
     ['{5F789EE2-7243-47D9-B950-F8B638DD3076}']
@@ -54,12 +54,12 @@ type
     procedure gc; cdecl;
     function getLocalizedInputStream(stream: JInputStream): JInputStream; cdecl;
     function getLocalizedOutputStream(stream: JOutputStream): JOutputStream; cdecl;
-    function exec(prog: JString): Jlang_Process; cdecl; overload;
-    function exec(prog: JString; envp: TJavaObjectArray<JString>): Jlang_Process; cdecl; overload;
-    function exec(prog: JString; envp: TJavaObjectArray<JString>; directory: JFile): Jlang_Process; cdecl; overload;
-    function exec(progArray: TJavaObjectArray<JString>): Jlang_Process; cdecl; overload;
-    function exec(progArray: TJavaObjectArray<JString>; envp: TJavaObjectArray<JString>): Jlang_Process; cdecl; overload;
-    function exec(progArray: TJavaObjectArray<JString>; envp: TJavaObjectArray<JString>; directory: JFile): Jlang_Process; cdecl; overload;
+    function exec(prog: JString): JProcess; cdecl; overload;
+    function exec(prog: JString; envp: TJavaObjectArray<JString>): JProcess; cdecl; overload;
+    function exec(prog: JString; envp: TJavaObjectArray<JString>; directory: JFile): JProcess; cdecl; overload;
+    function exec(progArray: TJavaObjectArray<JString>): JProcess; cdecl; overload;
+    function exec(progArray: TJavaObjectArray<JString>; envp: TJavaObjectArray<JString>): JProcess; cdecl; overload;
+    function exec(progArray: TJavaObjectArray<JString>; envp: TJavaObjectArray<JString>; directory: JFile): JProcess; cdecl; overload;
     procedure exit(code: Integer); cdecl;
     function freeMemory: Int64; cdecl;
     procedure halt(code: Integer); cdecl;
