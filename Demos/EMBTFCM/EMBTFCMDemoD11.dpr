0fde90675f3cb32d22a644a91ec34d078949e275
Dave Nottage <davidn@radsoft.com.au>
2022-02-04 22:27:04 +1030

    Added Delphi 11 version of project

---
+++ Demos/EMBTFCM/EMBTFCMDemoD11.dpr
@@ -0,0 +1,14 @@
+program EMBTFCMDemoD11;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  EF.View.Main in 'Views\EF.View.Main.pas' {MainView};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainView, MainView);
+  Application.Run;
+end.
