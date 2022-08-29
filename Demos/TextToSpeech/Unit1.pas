70638aba03525c14f289e60db5906d9196a5aaac
Dave Nottage <davidn@radsoft.com.au>
2022-08-29 09:42:47 +0930

    Added support for language voices

--- Demos/TextToSpeech/Unit1.pas
+++ Demos/TextToSpeech/Unit1.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls,
   FMX.Memo.Types, FMX.ScrollBox, FMX.Memo,
-  DW.TextToSpeech;
+  DW.TextToSpeech, FMX.ListBox;
 
 type
   TForm1 = class(TForm)
@@ -15,8 +15,13 @@ type
     StopButton: TButton;
     TextMemo: TMemo;
     LogMemo: TMemo;
+    LangComboBox: TComboBox;
+    LangLayout: TLayout;
+    UseExampleCheckBox: TCheckBox;
     procedure SpeakButtonClick(Sender: TObject);
     procedure StopButtonClick(Sender: TObject);
+    procedure LangComboBoxChange(Sender: TObject);
+    procedure UseExampleCheckBoxChange(Sender: TObject);
   private
     FSpeaker: TTextToSpeech;
     procedure SpeakerSpeechStartedHandler(Sender: TObject);
@@ -33,6 +38,14 @@ implementation
 
 {$R *.fmx}
 
+const
+  cExampleText: array[0..3] of string = (
+    'Now is the time for all good men to come to the aid of their country',
+    'Il est maintenant temps pour tous les hommes de bien de venir en aide à leur pays',
+    'Jetzt ist es an der Zeit, dass alle guten Männer ihrem Land zu Hilfe kommen',
+    'Ora è il momento per tutti gli uomini buoni di venire in aiuto del loro paese'
+  );
+
 { TForm1 }
 
 constructor TForm1.Create(AOwner: TComponent);
@@ -41,12 +54,20 @@ begin
   FSpeaker := TTextToSpeech.Create;
   FSpeaker.OnSpeechStarted := SpeakerSpeechStartedHandler;
   FSpeaker.OnSpeechFinished := SpeakerSpeechFinishedHandler;
+  TextMemo.Text := cExampleText[LangComboBox.ItemIndex];
 end;
 
 destructor TForm1.Destroy;
 begin
   FSpeaker.Free;
   inherited;
+end;
+
+procedure TForm1.LangComboBoxChange(Sender: TObject);
+begin
+  FSpeaker.Language := LangComboBox.Items[LangComboBox.ItemIndex];
+  if UseExampleCheckBox.IsChecked then
+    TextMemo.Text := cExampleText[LangComboBox.ItemIndex];
 end;
 
 procedure TForm1.SpeakButtonClick(Sender: TObject);
@@ -57,6 +78,12 @@ end;
 procedure TForm1.StopButtonClick(Sender: TObject);
 begin
   FSpeaker.Stop;
+end;
+
+procedure TForm1.UseExampleCheckBoxChange(Sender: TObject);
+begin
+  if not UseExampleCheckBox.IsChecked then
+    TextMemo.Text := '';
 end;
 
 procedure TForm1.SpeakerSpeechFinishedHandler(Sender: TObject);
