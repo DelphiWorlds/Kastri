7bb631b0cb50fb5662e1fe290b0f3845c8d3b199
Dave Nottage <davidn@radsoft.com.au>
2023-04-03 08:15:53 +0930

    Updated demo to use new features

--- Demos/WebBrowserFileChooser/Unit1.pas
+++ Demos/WebBrowserFileChooser/Unit1.pas
@@ -5,13 +5,15 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.WebBrowser,
-  DW.WebChromeClient.Android;
+  DW.WebChromeClient, FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TForm1 = class(TForm)
     WebBrowser: TWebBrowser;
+    UseCameraCheckBox: TCheckBox;
   private
     FManager: TWebChromeClientManager;
+    procedure ManagerFileChooserHandler(Sender: TObject; const AMimeTypes: TArray<string>; var AFileChooserKind: TFileChooserKind);
   public
     constructor Create(AOwner: TComponent); override;
   end;
@@ -24,8 +26,7 @@ implementation
 {$R *.fmx}
 
 uses
-  Androidapi.JNI.Webkit,
-  DW.Androidapi.JNI.DWWebChromeClient;
+  System.IOUtils;
 
 { TForm1 }
 
@@ -33,10 +34,39 @@ constructor TForm1.Create(AOwner: TComponent);
 begin
   inherited;
   FManager := TWebChromeClientManager.Create(WebBrowser);
+  // FileCachePath is a folder where files of photos that are taken are stored. If you do not specify a path, the camera roll area is used
+  // Using a value for FileCachePath gives an opportunity to control whether photos that are uploaded can be easily deleted
+  FManager.FileCachePath := TPath.Combine(TPath.GetTempPath, 'Cache');
+  FManager.OnFileChooser := ManagerFileChooserHandler;
   // https://ps.uci.edu/~franklin/doc/file_upload.html // <---- This URL and the one just below are for demoing the file chooser fix
-  // http://lacliniccaonline.com.br/resultados2020/mobile
+  // https://west-wind.com/wconnect/wcscripts/fileupload.wwd 
   // https://www.youtube.com/watch?v=QkBvmv8kt4U // <----- This URL is for demo-ing YouTube in full screen mode
-  WebBrowser.Navigate('https://www.youtube.com/watch?v=QkBvmv8kt4U');
+  WebBrowser.Navigate('https://www.w3docs.com/tools/code-editor/11957');
+end;
+
+procedure TForm1.ManagerFileChooserHandler(Sender: TObject; const AMimeTypes: TArray<string>; var AFileChooserKind: TFileChooserKind);
+var
+  LMimeType: string;
+  LHasImageType: Boolean;
+begin
+  LHasImageType := False;
+  for LMimeType in AMimeTypes do
+  begin
+    if LMimeType.StartsWith('image/') then
+    begin
+      LHasImageType := True;
+      Break;
+    end;
+  end;
+  if LHasImageType then
+  begin
+    // NOTE: Set AFileChooserKind to TFileChooserKind.VisualMedia if you want the user to be able to select photos/videos from their gallery ONLY
+    if UseCameraCheckBox.IsChecked then
+      AFileChooserKind := TFileChooserKind.Camera
+    else
+      AFileChooserKind := TFileChooserKind.VisualMedia;
+  end;
+
 end;
 
 end.
