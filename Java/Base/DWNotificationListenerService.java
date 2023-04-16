fcbc400a4bf379f93d7d3a027f38d30da7430ff5
Dave Nottage <davidn@radsoft.com.au>
2023-04-16 12:11:04 +0930

    Added source for DWNotificationListenerService

---
+++ Java/Base/DWNotificationListenerService.java
@@ -0,0 +1,53 @@
+package com.delphiworlds.kastri;
+
+/*******************************************************
+ *                                                     *
+ *                     Kastri                          *
+ *                                                     *
+ *        Delphi Worlds Cross-Platform Library         *
+ *                                                     *
+ * Copyright 2020-2023 Dave Nottage under MIT license  *
+ * which is located in the root folder of this library *
+ *                                                     *
+ *******************************************************/
+ 
+import android.content.Intent;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+public class DWNotificationListenerService extends NotificationListenerService {
+
+  private static final String TAG = "DWNotificationListenerService";
+
+  public static final String ACTION_NOTIFICATION_POSTED = "ACTION_NOTIFICATION_POSTED";
+  public static final String ACTION_NOTIFICATION_REMOVED = "ACTION_NOTIFICATION_REMOVED";
+  public static final String EXTRA_NOTIFICATION = "EXTRA_NOTIFICATION";
+
+  @Override
+  public void onListenerConnected() {
+    
+  }
+
+  @Override
+  public void onListenerDisconnected() {
+    
+  }
+
+  public void sendNotification(String action, StatusBarNotification sbn) {
+    Intent intent = new Intent(action);
+    intent.putExtra(DWNotificationListenerService.EXTRA_NOTIFICATION, sbn.getNotification().extras);
+    LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+  }
+
+  @Override
+  public void onNotificationPosted(StatusBarNotification sbn) {
+    sendNotification(DWNotificationListenerService.ACTION_NOTIFICATION_POSTED, sbn);
+  }
+
+  @Override
+  public void onNotificationRemoved(StatusBarNotification sbn) {
+    sendNotification(DWNotificationListenerService.ACTION_NOTIFICATION_REMOVED, sbn);
+  }
+
+}
