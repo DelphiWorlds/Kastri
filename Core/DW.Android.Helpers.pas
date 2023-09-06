c2324a556323a671d8c7ddbd4c04ee67810217cf
Dave Nottage <davidn@radsoft.com.au>
2023-09-07 07:11:09 +0930

    Various
    Fix memory leak
    Add OnSuccessListener
    Refactoring

--- Core/DW.Android.Helpers.pas
+++ Core/DW.Android.Helpers.pas
@@ -304,16 +304,40 @@ type
     constructor Create(const ARunHandler: TThreadProcedure; const ASync: Boolean = True);
   end;
 
-  TPlayCoreOnCompleteMethod = procedure(const ATask: JTask) of object;
+  TPlayCoreOnCompleteProc = reference to procedure(const ATask: JTask);
 
   TPlayCoreOnCompleteListener = class(TJavaLocal, JOnCompleteListener)
   private
-    FOnCompleteHandler: TPlayCoreOnCompleteMethod;
+    FHandler: TPlayCoreOnCompleteProc;
   public
     { JOnCompleteListener }
     procedure onComplete(task: JTask); cdecl;
   public
-    constructor Create(const AOnCompleteHandler: TPlayCoreOnCompleteMethod);
+    constructor Create(const AHandler: TPlayCoreOnCompleteProc);
+  end;
+
+  TPlayCoreOnSuccessProc = reference to procedure(const Obj: JObject);
+
+  TPlayCoreOnSuccessListener = class(TJavaLocal, JOnSuccessListener)
+  private
+    FHandler: TPlayCoreOnSuccessProc;
+  public
+    { JOnSuccessListener }
+    procedure onSuccess(result: JObject); cdecl;
+  public
+    constructor Create(const AHandler: TPlayCoreOnSuccessProc);
+  end;
+
+  TPlayCoreOnFailureProc = reference to procedure(const exception: JException);
+
+  TPlayCoreOnFailureListener = class(TJavaLocal, JOnFailureListener)
+  private
+    FHandler: TPlayCoreOnFailureProc;
+  public
+    { JOnFailureListener }
+    procedure onFailure(exception: JException); cdecl;
+  public
+    constructor Create(const AHandler: TPlayCoreOnFailureProc);
   end;
 
   TAndroidFileStream = class(TBytesStream)
@@ -894,13 +918,19 @@ end;
 class function TJImageHelper.JImageToByteArray(const AImage: JImage): TJavaArray<Byte>;
 var
   LBuffer: JByteBuffer;
+  LPlanes: TJavaObjectArray<JImage_Plane>;
 begin
   Result := nil;
   if AImage.getFormat = TJImageFormat.JavaClass.JPEG then
   begin
-    LBuffer := AImage.getPlanes.Items[0].getBuffer;
-    Result := TJavaArray<Byte>.Create(LBuffer.capacity);
-    LBuffer.get(Result);
+    LPlanes := AImage.getPlanes;
+    try
+      LBuffer := LPlanes.Items[0].getBuffer;
+      Result := TJavaArray<Byte>.Create(LBuffer.capacity);
+      LBuffer.get(Result);
+    finally
+      LPlanes.Free;
+    end;
   end
   else if AImage.getFormat = TJImageFormat.JavaClass.YUV_420_888 then
     Result := NV21ToJPEG(YUV_420_888ToNV21(AImage), AImage.getWidth, AImage.getHeight);
@@ -1019,15 +1049,44 @@ end;
 
 { TPlayCoreOnCompleteListener }
 
-constructor TPlayCoreOnCompleteListener.Create(const AOnCompleteHandler: TPlayCoreOnCompleteMethod);
+constructor TPlayCoreOnCompleteListener.Create(const AHandler: TPlayCoreOnCompleteProc);
 begin
-  FOnCompleteHandler := AOnCompleteHandler;
+  inherited Create;
+  FHandler := AHandler;
 end;
 
 procedure TPlayCoreOnCompleteListener.onComplete(task: JTask);
 begin
-  if Assigned(FOnCompleteHandler) then
-    FOnCompleteHandler(task);
+  if Assigned(FHandler) then
+    FHandler(task);
+end;
+
+{ TPlayCoreSuccessListener }
+
+constructor TPlayCoreOnSuccessListener.Create(const AHandler: TPlayCoreOnSuccessProc);
+begin
+  inherited Create;
+  FHandler := AHandler;
+end;
+
+procedure TPlayCoreOnSuccessListener.onSuccess(result: JObject);
+begin
+  if Assigned(FHandler) then
+    FHandler(result);
+end;
+
+{ TPlayCoreOnFailureListener }
+
+constructor TPlayCoreOnFailureListener.Create(const AHandler: TPlayCoreOnFailureProc);
+begin
+  inherited Create;
+  FHandler := AHandler;
+end;
+
+procedure TPlayCoreOnFailureListener.onFailure(exception: JException);
+begin
+  if Assigned(FHandler) then
+    FHandler(exception);
 end;
 
 { TAndroidFileStream }
