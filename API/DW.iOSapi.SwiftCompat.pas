5cbf271f8b1921b8c96b7adfc5c2abf2164f4c41
Dave Nottage <davidn@radsoft.com.au>
2023-10-09 09:37:25 +1030

    Added unit for Swift compatibility

---
+++ API/DW.iOSapi.SwiftCompat.pas
@@ -0,0 +1,40 @@
+unit DW.iOSapi.SwiftCompat;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{  Copyright 2020-2023 Dave Nottage under MIT license   }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+interface
+
+implementation
+
+procedure libswiftCompatibility50Loader; cdecl; external '/usr/lib/swift/iphoneos/libswiftCompatibility50.a';
+procedure libswiftCompatibility51Loader; cdecl; external '/usr/lib/swift/iphoneos/libswiftCompatibility51.a';
+procedure libswiftCompatibilityConcurrencyLoader; cdecl; external '/usr/lib/swift/iphoneos/libswiftCompatibilityConcurrency.a';
+procedure libswiftCompatibilityDynamicReplacementsLoader; cdecl; external '/usr/lib/swift/iphoneos/libswiftCompatibilityDynamicReplacements.a';
+procedure libswift_Concurrency; cdecl; external '/usr/lib/swift/libswift_Concurrency.dylib';
+procedure libswift_StringProcessingLoader; cdecl; external '/usr/lib/swift/libswift_StringProcessing.dylib';
+procedure libswiftCoreLoader; cdecl; external '/usr/lib/swift/libswiftCore.dylib';
+procedure libswiftCoreFoundationLoader; cdecl; external '/usr/lib/swift/libswiftCoreFoundation.dylib';
+procedure libswiftCoreGraphicsLoader; cdecl; external '/usr/lib/swift/libswiftCoreGraphics.dylib';
+procedure libswiftCoreImageLoader; cdecl; external '/usr/lib/swift/libswiftCoreImage.dylib';
+procedure libswiftDarwinLoader; cdecl; external '/usr/lib/swift/libswiftDarwin.dylib';
+procedure libswiftDataDetectionLoader; cdecl; external '/usr/lib/swift/libswiftDataDetection.dylib';
+procedure libswiftDispatchLoader; cdecl; external '/usr/lib/swift/libswiftDispatch.dylib';
+procedure libswiftFileProviderLoader; cdecl; external '/usr/lib/swift/libswiftFileProvider.dylib';
+procedure libswiftFoundationLoader; cdecl; external '/usr/lib/swift/libswiftFoundation.dylib';
+procedure libswiftMetalLoader; cdecl; external '/usr/lib/swift/libswiftMetal.dylib';
+procedure libswiftObjectiveCLoader; cdecl; external '/usr/lib/swift/libswiftObjectiveC.dylib';
+procedure libswiftQuartzCoreLoader; cdecl; external '/usr/lib/swift/libswiftQuartzCore.dylib';
+procedure libswiftUIKitLoader; cdecl; external '/usr/lib/swift/libswiftUIKit.dylib';
+
+end.
