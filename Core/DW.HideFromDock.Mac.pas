4409be6b1a6c055d5079a3634b6ee43c62c34cca
Dave Nottage <davidn@radsoft.com.au>
2023-10-18 12:13:00 +1030

    Added HideFromDock

---
+++ Core/DW.HideFromDock.Mac.pas
@@ -0,0 +1,35 @@
+unit DW.HideFromDock.Mac;
+
+{*******************************************************}
+{                                                       }
+{                      Kastri                           }
+{                                                       }
+{         Delphi Worlds Cross-Platform Library          }
+{                                                       }
+{  Copyright 2020-2023 Dave Nottage under MIT license   }
+{  which is located in the root folder of this library  }
+{                                                       }
+{*******************************************************}
+
+{$I DW.GlobalDefines.inc}
+
+// Including this unit in your macOS application turns it into an "accessory", i.e. it will be hidden from the Dock, as per:
+//   https://stackoverflow.com/a/9220857/3164070
+
+interface
+
+implementation
+
+uses
+  // macOS
+  Macapi.AppKit;
+
+function SharedApplication: NSApplication;
+begin
+  Result := TNSApplication.Wrap(TNSApplication.OCClass.sharedApplication);
+end;
+
+initialization
+  SharedApplication.setActivationPolicy(NSApplicationActivationPolicyAccessory);
+
+end.
