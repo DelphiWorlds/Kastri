7f4135ec59c87303cd347f19575eb05c722ed4f0
Dave Nottage <davidn@radsoft.com.au>
2024-01-30 06:52:15 +1030

    Fix compiler define

--- API/DW.iOSapi.MLKitCommon.pas
+++ API/DW.iOSapi.MLKitCommon.pas
@@ -109,20 +109,14 @@ implementation
 procedure AccelerateLoader; cdecl; external framework 'Accelerate';
 procedure MLKitCommonLoader; cdecl; external framework libMLKitCommon;
 procedure CLangRTLoader; cdecl; external '/usr/lib/clang/lib/darwin/libclang_rt.ios.a';
-{$IF Defined(FIREBASE)}
-procedure GoogleDataTransportLoader; cdecl; external framework 'GoogleDataTransport';
-procedure GoogleUtilitiesLoader; cdecl; external framework 'GoogleUtilities';
-procedure nanoPBLoader; cdecl; external framework 'nanoPB';
-procedure PromisesObjCLoader; cdecl; external framework 'PromisesObjC';
-{$ELSE}
+{$IF not Defined(FIREBASE)}
 procedure GoogleDataTransportLoader; cdecl; external 'libGoogleDataTransport.a';
 procedure GoogleUtilitiesLoader; cdecl; external 'libGoogleUtilities.a';
 procedure nanopbLoader; cdecl; external 'libnanopb.a';
-procedure PromisesObjCLoader; cdecl; external 'libPromisesObjC.a';
 {$ENDIF}
 procedure GoogleUtilitiesComponentsLoader; cdecl; external 'libGoogleUtilitiesComponents.a';
 procedure GoogleToolboxForMacLoader; cdecl; external 'libGoogleToolboxForMac.a';
 procedure GTMSessionFetcherLoader; cdecl; external 'libGTMSessionFetcher.a';
-procedure ProtobufLoader; cdecl; external 'libProtobuf.a';
+procedure PromisesObjCLoader; cdecl; external 'libPromisesObjC.a';
 
 end.
