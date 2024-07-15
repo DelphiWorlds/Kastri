399b5542e2194efb7ddfc819ce18e1b1f7cc03f1
Dave Nottage <davidn@radsoft.com.au>
2024-07-15 16:05:32 +0930

    Added D12 project

---
+++ Demos/Camera/CameraDemoD12.dpr
@@ -0,0 +1,16 @@
+program CameraDemoD12;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  CD.View.Main in 'Views\CD.View.Main.pas' {MainView},
+  CD.View.Camera in 'Views\CD.View.Camera.pas' {CameraView},
+  CD.Types in 'Core\CD.Types.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainView, MainView);
+  Application.Run;
+end.
