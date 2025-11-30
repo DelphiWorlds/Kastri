cb34ba1687549cf7f56f82f77b7231f007bffc2a
Dave Nottage <david.nottage@tcontinuity.com.au>
2025-12-01 09:47:03 +1030

    Added new constants

--- API/DW.Androidapi.JNI.DWMultiBroadcastReceiver.pas
+++ API/DW.Androidapi.JNI.DWMultiBroadcastReceiver.pas
@@ -23,16 +23,20 @@ type
   JDWMultiBroadcastReceiverClass = interface(JBroadcastReceiverClass)
     ['{7287006F-964B-4092-ACCB-9306009864A8}']
     {class} function _GetACTION_NOTIFICATION: JString; cdecl;
+    {class} function _GetACTION_NOTIFICATION_ACTION: JString; cdecl;
     {class} function _GetACTION_SERVICE_ALARM: JString; cdecl;
     {class} function _GetEXTRA_NOTIFICATION: JString; cdecl;
+    {class} function _GetEXTRA_NOTIFICATION_ACTION_NAME: JString; cdecl;
     {class} function _GetEXTRA_NOTIFICATION_ID: JString; cdecl;
     {class} function _GetEXTRA_NOTIFICATION_IMAGE: JString; cdecl;
     {class} function _GetEXTRA_NOTIFICATION_NAME: JString; cdecl;
     {class} function _GetEXTRA_NOTIFICATION_REPEATINTERVAL: JString; cdecl;
     {class} function init: JDWMultiBroadcastReceiver; cdecl;
     {class} property ACTION_NOTIFICATION: JString read _GetACTION_NOTIFICATION;
+    {class} property ACTION_NOTIFICATION_ACTION: JString read _GetACTION_NOTIFICATION_ACTION;
     {class} property ACTION_SERVICE_ALARM: JString read _GetACTION_SERVICE_ALARM;
     {class} property EXTRA_NOTIFICATION: JString read _GetEXTRA_NOTIFICATION;
+    {class} property EXTRA_NOTIFICATION_ACTION_NAME: JString read _GetEXTRA_NOTIFICATION_ACTION_NAME;
     {class} property EXTRA_NOTIFICATION_ID: JString read _GetEXTRA_NOTIFICATION_ID;
     {class} property EXTRA_NOTIFICATION_IMAGE: JString read _GetEXTRA_NOTIFICATION_IMAGE;
     {class} property EXTRA_NOTIFICATION_NAME: JString read _GetEXTRA_NOTIFICATION_NAME;
