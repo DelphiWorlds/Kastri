1460c1f62ec01dd78067eb36ff5884706cd044d9
Dave Nottage <david.nottage@tcontinuity.com.au>
2026-01-09 12:15:50 +1030

    Update copyright year

--- Java/Base/WebView/DWWebViewClientDelegate.java
+++ Java/Base/WebView/DWWebViewClientDelegate.java
@@ -6,7 +6,7 @@
  *                                                     *
  *        Delphi Worlds Cross-Platform Library         *
  *                                                     *
- * Copyright 2020-2024 Dave Nottage under MIT license  *
+ * Copyright 2020-2026 Dave Nottage under MIT license  *
  * which is located in the root folder of this library *
  *                                                     *
  *******************************************************/
--- Java/MediaProjection/DWMediaProjectionCallback.java
+++ Java/MediaProjection/DWMediaProjectionCallback.java
@@ -8,7 +8,7 @@ import com.delphiworlds.kastri.DWMediaProjectionCallbackDelegate;
  *                                                     *
  *        Delphi Worlds Cross-Platform Library         *
  *                                                     *
- * Copyright 2020-2024 Dave Nottage under MIT license  *
+ * Copyright 2020-2026 Dave Nottage under MIT license  *
  * which is located in the root folder of this library *
  *                                                     *
  *******************************************************/
