18e58ab2123c228160a4d3b7be9584ecd5e9e1ab
Dave Nottage <david.nottage@tcontinuity.com.au>
2026-09-07 11:02:44 +1000

    Add more dispatch methods

--- Core/DW.Macapi.Dispatch.pas
+++ Core/DW.Macapi.Dispatch.pas
@@ -19,13 +19,25 @@ interface
 
 uses
   // macOS
-  Macapi.Dispatch;
+  Macapi.CoreServices, Macapi.Dispatch;
+
+const
+{$IF Defined(IOS)}
+  libDispatch = '/usr/lib/libSystem.dylib';
+{$ELSE}
+  libDispatch = '/usr/lib/system/libdispatch.dylib';
+{$ENDIF}
+  DISPATCH_QUEUE_PRIORITY_DEFAULT = 0;
+  DISPATCH_TIME_NOW = 0;
+  NSEC_PER_MSEC:UInt64 = 1000000;
 
 type
   dispatch_work_t = reference to procedure;
-  dispatch_block_t = Pointer; // dispatch_work_t;
-  dispatch_source_t = Pointer;
-  dispatch_function_t = procedure(context: Pointer); cdecl;
+  dispatch_queue_t = dispatch_object_t;
+  dispatch_group_t = dispatch_object_t;
+  dispatch_source_t = dispatch_object_t;
+  dispatch_source_type_t = dispatch_object_t;
+  dispatch_block_t = Pointer;
   dispatch_data_t = Pointer;
 
   TGrandCentral = record
@@ -36,21 +48,27 @@ type
     class procedure DispatchAsync(const AProc: dispatch_work_t; const AQueue: dispatch_queue_t = 0); static;
   end;
 
+function dispatch_get_global_queue(priority: LongInt; flags: LongInt): dispatch_queue_t; cdecl;
+  external libDispatch name _PU + 'dispatch_get_global_queue';
+procedure dispatch_resume(source: dispatch_source_t); cdecl;
+  external libDispatch name _PU + 'dispatch_resume';
+function dispatch_source_create(&type: dispatch_source_type_t; handle: NativeUInt; mask: NativeUInt; queue: dispatch_queue_t): dispatch_source_t; cdecl;
+  external libDispatch name _PU + 'dispatch_source_create';
+procedure dispatch_source_set_event_handler(source: dispatch_source_t; handler: dispatch_block_t); cdecl;
+  external libDispatch name _PU + 'dispatch_source_set_event_handler';
+procedure dispatch_source_set_timer(source: dispatch_source_t; start: dispatch_time_t; interval: LongInt; leeway: LongInt); cdecl;
+  external libDispatch name _PU + 'dispatch_source_set_timer';
+function dispatch_time(when: dispatch_time_t; delta: Int64): dispatch_time_t; cdecl;
+  external libDispatch name _PU + 'dispatch_time';
+
 implementation
 
 uses
   // RTL
   System.SysUtils;
 
-const
-{$IF Defined(IOS)}
-  libdispatch = '/usr/lib/libSystem.dylib';
-{$ELSE}
-  libdispatch = '/usr/lib/system/libdispatch.dylib';
-{$ENDIF}
-
 procedure dispatch_sync_f(queue: dispatch_queue_t; context: Pointer; work: dispatch_function_t); cdecl;
-  external libdispatch name _PU + 'dispatch_sync_f';
+  external libDispatch name _PU + 'dispatch_sync_f';
 
 { Grand Central Dispatch implementation }
 
@@ -59,7 +77,7 @@ var
   LModule: HMODULE;
 begin
   Result := 0;
-  LModule := LoadLibrary(PWideChar(libdispatch));
+  LModule := LoadLibrary(PWideChar(libDispatch));
   if LModule <> 0 then
   try
     Result := dispatch_queue_t(GetProcAddress(LModule, PWideChar('_dispatch_main_q')));
